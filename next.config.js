dbda28277525ec7a80b41f71ecb419ab47a29d34
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-05-23 11:52:26 +0100

    Tweak next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: 'build',
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ['@google-cloud/datastore'],
+  },
+}
 
 module.exports = nextConfig
