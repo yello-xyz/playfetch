95e53bc66454c421dd9d4336d73695cc4d9f333b
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-05-24 19:16:43 +0100

    Send email with magic link on login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,11 +1,23 @@
 import { getUser } from '@/server/datastore'
+import { sendMail } from '@/server/email'
+import { buildURLForClientRoute } from '@/server/routing'
 import { withSessionRoute } from '@/server/session'
+import { sealData } from 'iron-session'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-async function login(req: NextApiRequest, res: NextApiResponse) {
-  req.session.user = await getUser(req.body.email)
-  await req.session.save()
-  res.json({})
+async function login(req: NextApiRequest, res: NextApiResponse<string>) {
+  const user = await getUser(req.body.email)
+  if (user) {
+    const { id, email, isAdmin } = user
+    req.session.user = { email, isAdmin }
+    await req.session.save()
+    const token = await sealData({ userID: id }, { password: process.env.TOKEN_SECRET ?? '', ttl: 15 * 60 })
+    const url = buildURLForClientRoute(`/${token}`, req.headers)
+    await sendMail(email, 'Log in to PlayFetch', `Log in: ${url}`, `<a href="${url}">Log in</a>`)
+    return res.json('Please check your email for a login link.')
+  } else {
+    return res.json('We could not find a user with that email address.')
+  }
 }
 
 export default withSessionRoute(login)
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,30 +1,32 @@
 import { Inter } from 'next/font/google'
 import { withLoggedOutSession } from '@/server/session'
-import { useRouter } from 'next/navigation'
 import api from '@/client/api'
 import LabeledTextInput from '@/client/labeledTextInput'
 import { useState } from 'react'
 import PendingButton from '@/client/pendingButton'
+import { Badge } from 'flowbite-react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const getServerSideProps = withLoggedOutSession(() => ({ props: {} }))
 
 export default function Login() {
-  const router = useRouter()
   const [email, setEmail] = useState('')
+  const [message, setMessage] = useState<string>()
 
   const login = async () => {
-    await api.login(email)
-    router.refresh()
+    setMessage(undefined)
+    const message = await api.login(email)
+    setMessage(message)
   }
 
   return (
     <main className={`flex flex-col gap-4 p-10 items-start ${inter.className}`}>
+      {message && <Badge>{message}</Badge>}
       <LabeledTextInput
         type='email'
         label='Email'
-        placeholder='Enter your email address...'
+        placeholder='Please enter your email address...'
         value={email}
         setValue={setEmail}
       />
