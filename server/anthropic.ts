99ef363044a0c21f87b02295629874a7308edcc9
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-06 16:23:21 +0100

    Add support for Anthropic

---
+++ server/anthropic.ts
@@ -0,0 +1,33 @@
+const headers = {
+  'x-api-key': process.env.ANTHROPIC_API_KEY ?? '',
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+}
+
+const calculateCost = (prompt: string, result: string) =>
+  (prompt.length * 4.6) / 1000000 + (result.length * 13.8) / 1000000
+
+  export default async function predict(prompt: string, temperature: number, maxTokens: number) {
+    try {
+    const url = new URL('https://api.anthropic.com/v1/complete')
+    const formattedPrompt = `\n\nHuman: ${prompt}\n\nAssistant:`
+    const body = JSON.stringify({
+      prompt: formattedPrompt,
+      temperature,
+      stream: false,
+      model: 'claude-v1',
+      max_tokens_to_sample: maxTokens,
+      stop_sequences: ['Human:'],
+    })
+    const response = await fetch(url, { method: 'POST', headers, body }).then(result => result.json())
+    if (response.exception) {
+      throw new Error(response.exception)
+    }
+    const cost = calculateCost(formattedPrompt, response.completion)
+    console.log(`Anthropic cost ~ $${cost.toFixed(3)}`)
+    return response?.completion
+  } catch (error) {
+    console.error(error)
+    return undefined
+  }
+}
