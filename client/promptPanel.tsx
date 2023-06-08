eec4ea913d943f5ba3cb5f358acc779f5e9c538d
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-08 12:45:33 +0100

    Add dynamic fields for input variables

--- client/promptPanel.tsx
+++ client/promptPanel.tsx
@@ -3,7 +3,7 @@ import LabeledTextInput from './labeledTextInput'
 import { RunConfig, Version } from '@/types'
 import TagsInput from './tagsInput'
 import PendingButton from './pendingButton'
-import { Dropdown, Label, RangeSlider } from 'flowbite-react'
+import { Dropdown, Label, RangeSlider, TextInput } from 'flowbite-react'
 import sanitizeHtml from 'sanitize-html'
 import ContentEditable from 'react-contenteditable'
 import { ContentEditableEvent } from 'react-contenteditable'
@@ -56,7 +56,7 @@ export default function PromptPanel({
 
   const contentEditableRef = useRef<HTMLElement>(null)
   const htmlContent = prompt
-    .replaceAll(/{{(.*?)}}/g, '<b>$1</b>')
+    .replaceAll(/{{([^{]*?)}}/g, '<b>$1</b>')
     .replaceAll(/\n(.*)$/gm, '<div>$1</div>')
     .replaceAll('<div></div>', '<div><br /></div>')
 
@@ -70,11 +70,39 @@ export default function PromptPanel({
         .replaceAll(/<div>(.*?)<\/div>/g, '\n$1')
         .replaceAll(/<b>(.*?)<\/b>/g, '{{$1}}')
         .replaceAll('{{}}', '')
+        .replace(/[\u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g, ' ')
+        .replaceAll(/{{(.*?) }}([^ ])/g, '{{$1}} $2')
+        .replaceAll(/([^ ]){{ (.*?)}}/g, '$1 {{$2}}')
     )
   }
 
+  const [inputState, setInputState] = useState<{ [key: string]: string }>({})
+  const inputVariables = prompt.match(/{{(.*?)}}/g)?.map(match => match.replace(/{{(.*?)}}/g, '$1')) ?? []
+  const promptInstance = inputVariables.reduce(
+    (prompt, variable) => prompt.replace(`{{${variable}}}`, inputState[variable] ?? ''),
+    prompt
+  )
+
   return (
     <div className='flex flex-col flex-1 gap-4 p-8 overflow-y-auto text-gray-500 max-w-prose'>
+      {inputVariables.length > 0 && (
+        <div className='flex flex-col gap-2'>
+          <Label value='Inputs' />
+          {inputVariables.map((variable, index) => (
+            <div key={index} className='flex gap-2'>
+              <Label className='flex-1' value={variable} htmlFor={variable} />
+              <TextInput
+                className='flex-1'
+                sizing='sm'
+                value={inputState[variable] ?? ''}
+                onChange={event => setInputState({ ...inputState, [variable]: event.target.value })}
+                id={variable}
+                required={true}
+              />
+            </div>
+          ))}
+        </div>
+      )}
       <div className='self-stretch'>
         <div className='block mb-1'>
           <Label value='Prompt' onClick={() => contentEditableRef.current?.focus()} />
@@ -90,7 +118,9 @@ export default function PromptPanel({
       <LabeledTextInput id='title' label='Title (optional)' value={title} setValue={updateTitle} />
       <TagsInput label='Tags (optional)' tags={tags} setTags={updateTags} />
       <div className='flex gap-2'>
-        <PendingButton disabled={!prompt.length} onClick={() => onRun(prompt, { provider, temperature, maxTokens })}>
+        <PendingButton
+          disabled={!prompt.length}
+          onClick={() => onRun(promptInstance, { provider, temperature, maxTokens })}>
           Run
         </PendingButton>
         <PendingButton disabled={!isDirty} onClick={onSave}>
