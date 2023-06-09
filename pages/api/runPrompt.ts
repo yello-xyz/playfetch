2eea94b58d799e3d91baca3e0bfc3b0607a0f1be
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-09 17:55:40 +0100

    Factor out running prompt with config

--- pages/api/runPrompt.ts
+++ pages/api/runPrompt.ts
@@ -6,26 +6,30 @@ import { withLoggedInSessionRoute } from '@/server/session'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { RunConfig } from '@/types'
 
-const getPredictor = (provider: string) => {
-  switch (provider) {
-    default:
-    case 'openai':
-      return openai
-    case 'anthropic':
-      return anthropic
-    case 'google':
-      return vertexai
+export const runPromptWithConfig = (prompt: string, config: RunConfig) => {
+  const resolvedPrompt = Object.entries(config.inputs).reduce(
+    (prompt, [variable, value]) => prompt.replaceAll(`{{${variable}}}`, value),
+    prompt
+  )
+
+  const getPredictor = (provider: string) => {
+    switch (provider) {
+      default:
+      case 'openai':
+        return openai
+      case 'anthropic':
+        return anthropic
+      case 'google':
+        return vertexai
+    }
   }
+
+  return getPredictor(config.provider)(resolvedPrompt, config.temperature, config.maxTokens)
 }
 
 async function runPrompt(req: NextApiRequest, res: NextApiResponse) {
   const config: RunConfig = req.body.config
-  const resolvedPrompt = Object.entries(config.inputs).reduce(
-    (prompt, [variable, value]) => prompt.replaceAll(`{{${variable}}}`, value),
-    req.body.prompt
-  )
-  
-  const { output, cost } = await getPredictor(config.provider)(resolvedPrompt, config.temperature, config.maxTokens)
+  const { output, cost } = await runPromptWithConfig(req.body.prompt, config)
   if (output?.length) {
     await saveRun(req.session.user!.id, req.body.promptID, req.body.versionID, output, config, cost)
   }
