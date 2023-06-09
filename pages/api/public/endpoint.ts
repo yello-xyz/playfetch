d3a3b48e1ed6a0136b248ede85096d34f13efa2c
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-09 17:59:36 +0100

    Run prompt in public endpoint (if valid api key)

--- pages/api/public/endpoint.ts
+++ pages/api/public/endpoint.ts
@@ -1,19 +1,24 @@
 import { ParseQuery } from '@/client/clientRoute'
 import { getEndpoint, getProjectIDFromURLPath } from '@/server/datastore'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { runPromptWithConfig } from '../runPrompt'
 
 async function endpoint(req: NextApiRequest, res: NextApiResponse) {
-  const { project, endpoint } = ParseQuery(req.query)
+  const { project, endpoint: endpointName } = ParseQuery(req.query)
   const apiKey = (req.headers['x-api-key'] as string) ?? 'invalid-api-key'
 
   const projectID = await getProjectIDFromURLPath(project, apiKey)
 
   if (projectID) {
-    const endpointConfig = await getEndpoint(projectID, endpoint)
-    return res.json({ endpointConfig })
-  } else {
-    return res.status(401).json({ error: 'Invalid URL or API Key' })
+    const endpoint = await getEndpoint(projectID, endpointName)
+    if (endpoint) {
+      // TODO log output, cost, failures, etc.
+      const { output } = await runPromptWithConfig(endpoint.prompt, endpoint.config)
+      return res.json({ output })
+    }
   }
+
+  return res.status(401).json({ error: 'Invalid URL or API Key' })
 }
 
 export default endpoint
