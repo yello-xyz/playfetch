3d0824385f12cf826731671a84a6ef62e96efad5
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-15 10:45:05 +0100

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   content: [
     './node_modules/flowbite-react/**/*.js',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './client/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
