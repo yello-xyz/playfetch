753c4cd151079718aed53e7e8abf39079531d4ba
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-19 14:03:48 +0100

    Factor out PromptCell component

--- client/promptsGridView.tsx
+++ client/promptsGridView.tsx
@@ -16,32 +16,52 @@ export default function PromptsGridView({
   onSelect: (promptID: number) => void
   onRefresh: () => void
 }) {
+  return (
+    <div className='flex flex-wrap gap-6 p-6'>
+      {prompts.map((prompt, index) => (
+        <PromptCell prompt={prompt} index={index} onSelect={onSelect} onRefresh={onRefresh} />
+      ))}
+    </div>
+  )
+}
+
+function PromptCell({
+  prompt,
+  index,
+  onSelect,
+  onRefresh,
+}: {
+  prompt: Prompt
+  index: number
+  onSelect: (promptID: number) => void
+  onRefresh: () => void
+}) {
   const [isMenuExpanded, setIsMenuExpanded] = useState(false)
 
   return (
-    <div className='flex flex-wrap gap-6 p-6'>
-      {prompts.map((prompt, promptIndex) => (
-        <div
-          className={`flex flex-col gap-1 p-4 border border-gray-300 rounded-lg cursor-pointer w-96 h-60`}
-          key={promptIndex}
-          onClick={() => onSelect(prompt.id)}>
-          <div className='flex items-start justify-between gap-2'>
-            <span className='flex-1 text-sm font-medium line-clamp-2'>{prompt.name}</span>
-            <div className='flex'>
-              <IconButton
-                icon={prompt.favorited ? filledStarIcon.src : starIcon.src}
-                onClick={() => api.toggleFavorite(prompt.id, !prompt.favorited).then(onRefresh)}
-              />
-              <IconButton icon={dotsIcon.src} onClick={() => setIsMenuExpanded(!isMenuExpanded)} />
-              {isMenuExpanded && (
-                <PopupMenu expanded={isMenuExpanded} collapse={() => setIsMenuExpanded(false)}></PopupMenu>
-              )}
+    <div
+      className={`flex flex-col gap-1 p-4 border border-gray-300 rounded-lg cursor-pointer w-96 h-60`}
+      key={index}
+      onClick={() => onSelect(prompt.id)}>
+      <div className='flex items-start justify-between gap-2'>
+        <span className='flex-1 text-sm font-medium line-clamp-2'>{prompt.name}</span>
+        <div className='relative flex'>
+          <IconButton
+            icon={prompt.favorited ? filledStarIcon.src : starIcon.src}
+            onClick={() => api.toggleFavorite(prompt.id, !prompt.favorited).then(onRefresh)}
+          />
+          <IconButton icon={dotsIcon.src} onClick={() => setIsMenuExpanded(!isMenuExpanded)} />
+          {isMenuExpanded && (
+            <div className='absolute right-0 top-7'>
+              <PopupMenu expanded={isMenuExpanded} collapse={() => setIsMenuExpanded(false)}>
+                <PopupMenuItem destructive title='Delete' callback={() => {}} />
+              </PopupMenu>
             </div>
-          </div>
-          <span className='text-xs text-gray-500'>Edited {FormatRelativeDate(prompt.timestamp)}</span>
-          <span className='mt-3 text-xs text-gray-500 line-clamp-[9]'>{prompt.prompt}</span>
+          )}
         </div>
-      ))}
+      </div>
+      <span className='text-xs text-gray-500'>Edited {FormatRelativeDate(prompt.timestamp)}</span>
+      <span className='mt-3 text-xs text-gray-500 line-clamp-[9]'>{prompt.prompt}</span>
     </div>
   )
 }
