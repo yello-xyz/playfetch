5d12539a87ef947c589b2464402dca12127294fc
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-19 13:12:28 +0100

    Add favourite button in prompt cell

--- client/promptsGridView.tsx
+++ client/promptsGridView.tsx
@@ -1,5 +1,7 @@
 import { FormatRelativeDate, Truncate } from '@/common/formatting'
 import { Prompt } from '@/types'
+import starIcon from '@/public/star.svg'
+import filledStarIcon from '@/public/filledStar.svg'
 
 export default function PromptsGridView({
   prompts = [],
@@ -15,7 +17,10 @@ export default function PromptsGridView({
           className={`flex flex-col gap-1 p-4 border border-gray-300 rounded-lg cursor-pointer w-96 h-60`}
           key={promptIndex}
           onClick={() => onSelect(prompt.id)}>
-          <span className='text-sm font-medium'>{prompt.name}</span>
+          <div className='flex items-center justify-between'>
+            <span className='text-sm font-medium'>{prompt.name}</span>
+            <IconButton icon={prompt.favorited ? filledStarIcon.src : starIcon.src} onClick={() => {}} />
+          </div>
           <span className='text-xs text-gray-500'>Edited {FormatRelativeDate(prompt.timestamp)}</span>
           <span className='mt-3 text-xs text-gray-500'>{Truncate(prompt.prompt, 500)}</span>
         </div>
@@ -23,3 +28,7 @@ export default function PromptsGridView({
     </div>
   )
 }
+
+const IconButton = ({ icon, onClick }: { icon: string; onClick: () => void }) => (
+  <img className='w-6 h-6 rounded cursor-pointer hover:bg-gray-100' src={icon} onClick={onClick} />
+)
