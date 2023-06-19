683544db7582b2b7090c285f8b32930cd9651a9f
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-19 13:48:02 +0100

    Add more action button in prompt grid

--- client/promptsGridView.tsx
+++ client/promptsGridView.tsx
@@ -2,7 +2,10 @@ import { FormatRelativeDate, Truncate } from '@/common/formatting'
 import { Prompt } from '@/types'
 import starIcon from '@/public/star.svg'
 import filledStarIcon from '@/public/filledStar.svg'
+import dotsIcon from '@/public/dots.svg'
 import api from './api'
+import { useState } from 'react'
+import PopupMenu, { PopupMenuItem } from './popupMenu'
 
 export default function PromptsGridView({
   prompts = [],
@@ -13,6 +16,8 @@ export default function PromptsGridView({
   onSelect: (promptID: number) => void
   onRefresh: () => void
 }) {
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false)
+
   return (
     <div className='flex flex-wrap gap-6 p-6'>
       {prompts.map((prompt, promptIndex) => (
@@ -20,15 +25,21 @@ export default function PromptsGridView({
           className={`flex flex-col gap-1 p-4 border border-gray-300 rounded-lg cursor-pointer w-96 h-60`}
           key={promptIndex}
           onClick={() => onSelect(prompt.id)}>
-          <div className='flex items-center justify-between'>
-            <span className='text-sm font-medium'>{prompt.name}</span>
-            <IconButton
-              icon={prompt.favorited ? filledStarIcon.src : starIcon.src}
-              onClick={() => api.toggleFavorite(prompt.id, !prompt.favorited).then(onRefresh)}
-            />
+          <div className='flex items-start justify-between gap-2'>
+            <span className='flex-1 text-sm font-medium line-clamp-2'>{prompt.name}</span>
+            <div className='flex'>
+              <IconButton
+                icon={prompt.favorited ? filledStarIcon.src : starIcon.src}
+                onClick={() => api.toggleFavorite(prompt.id, !prompt.favorited).then(onRefresh)}
+              />
+              <IconButton icon={dotsIcon.src} onClick={() => setIsMenuExpanded(!isMenuExpanded)} />
+              {isMenuExpanded && (
+                <PopupMenu expanded={isMenuExpanded} collapse={() => setIsMenuExpanded(false)}></PopupMenu>
+              )}
+            </div>
           </div>
           <span className='text-xs text-gray-500'>Edited {FormatRelativeDate(prompt.timestamp)}</span>
-          <span className='mt-3 text-xs text-gray-500'>{Truncate(prompt.prompt, 500)}</span>
+          <span className='mt-3 text-xs text-gray-500 line-clamp-[9]'>{prompt.prompt}</span>
         </div>
       ))}
     </div>
