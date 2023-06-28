e23a17a9a67fb62cc9fcef07623aeaa1ee894c85
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-28 14:19:49 +0100

    Show selection popup button in prompt input

--- client/promptInput.tsx
+++ client/promptInput.tsx
@@ -1,12 +1,32 @@
-import { MouseEvent } from 'react'
+import { RefObject, useEffect, useState } from 'react'
 import sanitizeHtml from 'sanitize-html'
 import ContentEditable from 'react-contenteditable'
 import { ContentEditableEvent } from 'react-contenteditable'
 import { FocusEvent } from 'react'
 import { useRef } from 'react'
-import { HiCodeBracketSquare } from 'react-icons/hi2'
 import Label from './label'
 import { ExtractPromptVariables } from '@/common/formatting'
+
+const extractSelection = (contentEditableRef: RefObject<HTMLElement>, containerRef: RefObject<HTMLElement>) => {
+  const selection = document.getSelection()
+  const selectionParent = selection?.anchorNode?.parentElement
+  if (selection && selectionParent?.textContent && containerRef.current) {
+    const isTopLevel = selectionParent === contentEditableRef.current
+    const isInput = !isTopLevel && selectionParent.parentElement === contentEditableRef.current
+    const text = isTopLevel ? selection.toString().trim() : selectionParent.textContent.trim()
+    if ((isTopLevel || isInput) && text.length > 0) {
+      const selectionRect = selection.getRangeAt(0).getBoundingClientRect()
+      const containerRect = containerRef.current.getBoundingClientRect()
+      const position = {
+        x: selectionRect.left - containerRect.left + selectionRect.width / 2,
+        y: selectionRect.top - containerRect.top - 42,
+      }
+      return { text, position, isInput }
+    }
+  }
+  return undefined
+}
+
 
 export default function PromptInput({
   prompt,
@@ -17,10 +37,32 @@ export default function PromptInput({
   setPrompt: (prompt: string) => void
   showInputs?: boolean
 }) {
-  const styling = 'class="text-white rounded px-1 py-0.5 bg-violet-500 font-normal"'
   const contentEditableRef = useRef<HTMLElement>(null)
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [selection, setSelection] = useState<{ text: string; position: { x: number; y: number }; isInput: boolean }>()
+
+  useEffect(() => {
+    const selectionChangeHandler = () => setSelection(extractSelection(contentEditableRef, containerRef))
+    if (showInputs) {
+      document.addEventListener('selectionchange', selectionChangeHandler)
+    }
+    return () => {
+      document.removeEventListener('selectionchange', selectionChangeHandler)
+    }
+  }, [contentEditableRef.current, containerRef.current])
+
+  const toggleInput = (text: string) => {
+    if (ExtractPromptVariables(prompt).includes(text)) {
+      setPrompt(prompt.replaceAll(`{{${text}}}`, text))
+    } else {
+      setPrompt(prompt.replaceAll(text, `{{${text}}}`))
+    }
+  }
+
+  const inputStyle = 'class="text-white rounded px-1 py-0.5 bg-violet-500 font-normal"'
   const htmlContent = prompt
-    .replaceAll(/{{([^{]*?)}}/g, `<b ${styling}>$1</b>`)
+    .replaceAll(/{{([^{]*?)}}/g, `<b ${inputStyle}>$1</b>`)
     .replaceAll(/\n(.*)$/gm, '<div>$1</div>')
     .replaceAll('<div></div>', '<div><br /></div>')
 
@@ -40,31 +82,30 @@ export default function PromptInput({
     )
   }
 
-  const inputs = ExtractPromptVariables(prompt)
-
-  const extractVariable = () => {
-    const selection = window.getSelection()?.toString()?.trim()
-    if (selection && selection.length > 0) {
-      if (inputs.includes(selection)) {
-        setPrompt(prompt.replaceAll(`{{${selection}}}`, selection))
-      } else {
-        setPrompt(prompt.replaceAll(selection, `{{${selection}}}`))
-      }
-    }
-  }
-
   return (
-    <div className='flex flex-col gap-2'>
+    <div ref={containerRef} className='relative flex flex-col gap-2'>
       <div className='flex items-center block gap-2 mb-1'>
         <Label onClick={() => contentEditableRef.current?.focus()}>Prompt</Label>
-        {showInputs && <HiCodeBracketSquare size={24} className='cursor-pointer' onMouseDown={extractVariable} />}
       </div>
       <ContentEditable
-        className='p-4 text-gray-800 border border-gray-300 rounded-lg'
+        className='p-4 text-gray-800 border border-gray-300 rounded-lg selection:bg-violet-300'
         onChange={updateHTMLContent}
         html={htmlContent}
         innerRef={contentEditableRef}
       />
+      {selection && (
+        <div
+          className='absolute flex items-center justify-center p-2 overflow-visible text-center max-w-0'
+          style={{ top: selection.position.y, left: selection.position.x }}>
+          <div className='p-1 bg-white rounded-lg shadow whitespace-nowrap'>
+            <div
+              className='px-1 rounded cursor-pointer hover:bg-gray-100'
+              onMouseDown={() => toggleInput(selection.text)}>
+              {selection.isInput ? 'Remove Input' : 'Create Input'}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
