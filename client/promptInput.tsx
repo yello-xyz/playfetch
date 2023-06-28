ae538fa199fd18375d2b66a074e054482039cb87
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-28 15:09:31 +0100

    Ensure we extract the right instance of the input
    In the case of removing an input, it's easy to replace all instances in the prompt, but when creating an input, we need to ensure we only create an input for the selected instance, and it's easier to do that through the HTML of the range rather than the prompt.

--- client/promptInput.tsx
+++ client/promptInput.tsx
@@ -1,11 +1,31 @@
 import { RefObject, useEffect, useState } from 'react'
 import sanitizeHtml from 'sanitize-html'
 import ContentEditable from 'react-contenteditable'
-import { ContentEditableEvent } from 'react-contenteditable'
-import { FocusEvent } from 'react'
 import { useRef } from 'react'
 import Label from './label'
-import { ExtractPromptVariables } from '@/common/formatting'
+import { StripPromptSentinels } from '@/common/formatting'
+
+const inputStyle = 'class="text-white rounded px-1 py-0.5 bg-violet-500 font-normal"'
+const toHTML = (text: string) =>
+  text
+    .replaceAll(/{{([^{]*?)}}/g, `<b ${inputStyle}>$1</b>`)
+    .replaceAll(/\n(.*)$/gm, '<div>$1</div>')
+    .replaceAll('<div></div>', '<div><br /></div>')
+
+const fromHTML = (html: string) =>
+  sanitizeHtml(html, {
+    allowedTags: ['br', 'div', 'b'],
+    allowedAttributes: { b: ['class'] },
+  })
+    .replaceAll('<br />', '')
+    .replaceAll(/<div>(.*?)<\/div>/g, '\n$1')
+    .replaceAll(/<b[^>]*>(.*?)<\/b>/g, '{{$1}}')
+    .replaceAll('{{}}', '')
+    .replace(/[\u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g, ' ')
+    .replaceAll(/{{(.*?)([ \.]+)}}([^ ])/g, '{{$1}}$2$3')
+    .replaceAll(/([^ ]){{([ \.]+)(.*?)}}/g, '$1$2{{$3}}')
+
+type Selection = { text: string; range: Range; popupPoint: { x: number; y: number }; isInput: boolean }
 
 const extractSelection = (contentEditableRef: RefObject<HTMLElement>, containerRef: RefObject<HTMLElement>) => {
   const selection = document.getSelection()
@@ -15,18 +35,18 @@ const extractSelection = (contentEditableRef: RefObject<HTMLElement>, containerR
     const isInput = !isTopLevel && selectionParent.parentElement === contentEditableRef.current
     const text = isTopLevel ? selection.toString().trim() : selectionParent.textContent.trim()
     if ((isTopLevel || isInput) && text.length > 0) {
-      const selectionRect = selection.getRangeAt(0).getBoundingClientRect()
+      const range = selection.getRangeAt(0)
+      const selectionRect = range.getBoundingClientRect()
       const containerRect = containerRef.current.getBoundingClientRect()
-      const position = {
+      const popupPoint = {
         x: selectionRect.left - containerRect.left + selectionRect.width / 2,
         y: selectionRect.top - containerRect.top - 42,
       }
-      return { text, position, isInput }
+      return { text, range, popupPoint, isInput }
     }
   }
   return undefined
 }
-
 
 export default function PromptInput({
   prompt,
@@ -40,7 +60,7 @@ export default function PromptInput({
   const contentEditableRef = useRef<HTMLElement>(null)
 
   const containerRef = useRef<HTMLDivElement>(null)
-  const [selection, setSelection] = useState<{ text: string; position: { x: number; y: number }; isInput: boolean }>()
+  const [selection, setSelection] = useState<Selection>()
 
   useEffect(() => {
     const selectionChangeHandler = () => setSelection(extractSelection(contentEditableRef, containerRef))
@@ -52,34 +72,12 @@ export default function PromptInput({
     }
   }, [contentEditableRef.current, containerRef.current])
 
-  const toggleInput = (text: string) => {
-    if (ExtractPromptVariables(prompt).includes(text)) {
+  const toggleInput = (text: string, range: Range, isInput: boolean) => {
+    if (isInput) {
       setPrompt(prompt.replaceAll(`{{${text}}}`, text))
     } else {
-      setPrompt(prompt.replaceAll(text, `{{${text}}}`))
+      range.surroundContents(document.createElement('b'))
     }
-  }
-
-  const inputStyle = 'class="text-white rounded px-1 py-0.5 bg-violet-500 font-normal"'
-  const htmlContent = prompt
-    .replaceAll(/{{([^{]*?)}}/g, `<b ${inputStyle}>$1</b>`)
-    .replaceAll(/\n(.*)$/gm, '<div>$1</div>')
-    .replaceAll('<div></div>', '<div><br /></div>')
-
-  const updateHTMLContent = (event: ContentEditableEvent | FocusEvent) => {
-    setPrompt(
-      sanitizeHtml(event.currentTarget.innerHTML, {
-        allowedTags: ['br', 'div', 'b'],
-        allowedAttributes: { b: ['class'] },
-      })
-        .replaceAll('<br />', '')
-        .replaceAll(/<div>(.*?)<\/div>/g, '\n$1')
-        .replaceAll(/<b[^>]*>(.*?)<\/b>/g, '{{$1}}')
-        .replaceAll('{{}}', '')
-        .replace(/[\u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g, ' ')
-        .replaceAll(/{{(.*?)([ \.]+)}}([^ ])/g, '{{$1}}$2$3')
-        .replaceAll(/([^ ]){{([ \.]+)(.*?)}}/g, '$1$2{{$3}}')
-    )
   }
 
   return (
@@ -89,18 +87,18 @@ export default function PromptInput({
       </div>
       <ContentEditable
         className='p-4 text-gray-800 border border-gray-300 rounded-lg selection:bg-violet-300'
-        onChange={updateHTMLContent}
-        html={htmlContent}
+        onChange={event => setPrompt(fromHTML(event.currentTarget.innerHTML))}
+        html={toHTML(prompt)}
         innerRef={contentEditableRef}
       />
       {selection && (
         <div
           className='absolute flex items-center justify-center p-2 overflow-visible text-center max-w-0'
-          style={{ top: selection.position.y, left: selection.position.x }}>
+          style={{ top: selection.popupPoint.y, left: selection.popupPoint.x }}>
           <div className='p-1 bg-white rounded-lg shadow whitespace-nowrap'>
             <div
               className='px-1 rounded cursor-pointer hover:bg-gray-100'
-              onMouseDown={() => toggleInput(selection.text)}>
+              onMouseDown={() => toggleInput(selection.text, selection.range, selection.isInput)}>
               {selection.isInput ? 'Remove Input' : 'Create Input'}
             </div>
           </div>
