0addd6be3d6637a5b5c43ecfd432d7d3ce08ac3f
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-29 13:23:25 +0100

    Workaround mismatched date formatting

--- client/runTimeline.tsx
+++ client/runTimeline.tsx
@@ -1,5 +1,6 @@
 import { FormatCost, FormatDate } from '@/common/formatting'
 import { Run } from '@/types'
+import { useEffect, useState } from 'react'
 
 export default function RunTimeline({ runs }: { runs: Run[] }) {
   return (
@@ -8,17 +9,29 @@ export default function RunTimeline({ runs }: { runs: Run[] }) {
       {runs.length > 0 ? (
         <div className='flex flex-col flex-1 gap-2 overflow-y-auto'>
           {runs.map((run, index) => (
-            <div key={index} className='flex flex-col gap-3 p-4 whitespace-pre-wrap rounded-lg bg-sky-50'>
-              {run.output}
-              <div className='self-end text-xs'>
-                {FormatCost(run.cost)} • {FormatDate(run.timestamp)}
-              </div>
-            </div>
+            <RunCell key={index} run={run} />
           ))}
         </div>
       ) : (
         <EmptyRuns />
       )}
+    </div>
+  )
+}
+
+function RunCell({ run }: { run: Run }) {
+  const [formattedDate, setFormattedDate] = useState<string>()
+  useEffect(() => {
+    setFormattedDate(FormatDate(run.timestamp))
+  }, [run.timestamp])
+
+
+  return (
+    <div className='flex flex-col gap-3 p-4 whitespace-pre-wrap rounded-lg bg-sky-50'>
+      {run.output}
+      <div className='self-end text-xs'>
+        {FormatCost(run.cost)} • {formattedDate}
+      </div>
     </div>
   )
 }
