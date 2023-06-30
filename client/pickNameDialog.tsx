7ec24fbe80fb9902faa41abe98b9b677c9c80f6d
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-06-30 18:07:22 +0100

    Clean up pick name dialog
    Don't need validator in this dialog anymore

--- client/pickNameDialog.tsx
+++ client/pickNameDialog.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useState } from 'react'
 import ModalDialog from './modalDialog'
 import TextInput from './textInput'
-import { debounce } from 'debounce'
 
 export default function PickNameDialog({
   title,
   confirmTitle,
   label,
   initialName,
-  validator,
   onConfirm,
   onDismiss,
 }: {
@@ -16,57 +14,21 @@ export default function PickNameDialog({
   confirmTitle: string
   label: string
   initialName?: string
-  validator?: (name: string) => Promise<{ url?: string }>
   onConfirm: (name: string) => void
   onDismiss: () => void
 }) {
-  const [name, setName] = useState('')
-  const [url, setURL] = useState(validator ? undefined : '')
-  const [isPending, setPending] = useState(false)
-
-  useEffect(() => {
-    if (initialName) {
-      updateName(initialName)
-    }
-  }, [prompt])
+  const [name, setName] = useState(initialName ?? '')
 
   const dialogPrompt = {
     title,
     confirmTitle,
     callback: () => onConfirm(name),
-    disabled: url === undefined || name.length === 0,
+    disabled: name.length === 0,
   }
-
-  const checkName = useMemo(
-    () =>
-      debounce((name: string) => {
-        validator?.(name).then(({ url }) => {
-          setURL(url)
-          setPending(false)
-        })
-      }),
-    [prompt]
-  )
-
-  const updateName = (name: string) => {
-    setName(name)
-    if (validator) {
-      setPending(true)
-      setURL(undefined)
-      checkName(name)
-    }
-  }
-
-  const isURLUnavailable = name.length > 0 && !isPending && url === undefined
 
   return (
     <ModalDialog prompt={dialogPrompt} onDismiss={onDismiss}>
-      <TextInput id='name' label={label} value={name} setValue={updateName} />
-      {validator && (
-        <div className={`${isURLUnavailable ? 'text-red-500' : 'text-gray-500'}`}>
-          {isURLUnavailable ? 'This name is not available.' : url ? url : <span>&nbsp;</span>}
-        </div>
-      )}
+      <TextInput id='name' label={label} value={name} setValue={setName} />
     </ModalDialog>
   )
 }
