b1c7e36cc9fa190a116962436b3dcaf95e0a7dd2
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-07-01 22:13:23 +0100

    Add Google provider for next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,11 +2,16 @@ import { getUserForEmail, markUserLogin } from '@/server/datastore/users'
 import NextAuth, { Session, User } from 'next-auth'
 import { JWT } from 'next-auth/jwt/types'
 import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
 
 const getRegisteredUser = async (email?: string | null) => email ? getUserForEmail(email) : undefined
 
 export const authOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,6 +3,7 @@ import { withLoggedOutSession } from '@/server/session'
 import { signIn } from 'next-auth/react'
 import ClientRoute from '@/client/clientRoute'
 import githubIcon from '@/public/github.svg'
+import googleIcon from '@/public/google.svg'
 
 const inter = Inter({ subsets: ['latin'], weight: ['400', '500', '600'] })
 
@@ -12,16 +13,23 @@ export default function Login() {
   return (
     <main className={`bg-gray-100 h-screen flex flex-col items-center justify-center gap-6 p-10 ${inter.className}`}>
       <span className='text-2xl font-semibold'>Sign in to Play/Fetch</span>
-      <div className='p-16 bg-white rounded-lg shadow'>
-        <button
-          className='flex justify-center bg-gray-100 hover:bg-gray-200 w-96 py-3.5 border border-gray-300 rounded'
-          onClick={() => signIn('github', { callbackUrl: ClientRoute.Home }).then()}>
-          <div className='flex items-center gap-2'>
-            <img className='w-[22px] h-[22px]' src={githubIcon.src} />
-            Sign in with Github
-          </div>
-        </button>
+      <div className='flex flex-col gap-3 p-16 bg-white rounded-lg shadow'>
+        <SignInButton name='Google' icon={googleIcon.src} provider='google' />
+        <SignInButton name='Github' icon={githubIcon.src} provider='github' />
       </div>
     </main>
+  )
+}
+
+function SignInButton({ name, icon, provider }: { name: string; icon: string; provider: string }) {
+  return (
+    <button
+    className='flex justify-center bg-gray-100 hover:bg-gray-200 w-96 py-3.5 border border-gray-300 rounded'
+    onClick={() => signIn(provider, { callbackUrl: ClientRoute.Home }).then()}>
+    <div className='flex items-center gap-2'>
+      <img className='w-[22px] h-[22px]' src={icon} />
+      Sign in with {name}
+    </div>
+  </button>
   )
 }
