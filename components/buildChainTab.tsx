ced202d8e57e4534a5ad7e8d7ccc10e371e6c77e
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-07-03 14:13:05 +0100

    Allow to map output to consecutive input

--- components/buildChainTab.tsx
+++ components/buildChainTab.tsx
@@ -1,12 +1,14 @@
 import { ActivePrompt, Prompt, Version } from '@/types'
-import { useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import DropdownMenu from './dropdownMenu'
 import api from '@/src/client/api'
 import VersionSelector from './versionSelector'
 import { ExtractPromptVariables } from '@/src/common/formatting'
 import Label from './label'
 
-type ChainItem = { prompt: Prompt; version: undefined } | { prompt: ActivePrompt; version: Version }
+type ChainItem =
+  | { prompt: Prompt; version: undefined; output: undefined }
+  | { prompt: ActivePrompt; version: Version; output?: string }
 
 export default function BuildChainTab({ prompts }: { prompts: Prompt[] }) {
   const [chain, setChain] = useState<ChainItem[]>([])
@@ -27,7 +29,7 @@ export default function BuildChainTab({ prompts }: { prompts: Prompt[] }) {
     const cachedPrompt = promptCache[promptID]
     return cachedPrompt
       ? { prompt: cachedPrompt, version: cachedPrompt.versions[0] }
-      : { prompt: prompts.find(prompt => prompt.id === promptID)!, version: undefined }
+      : { prompt: prompts.find(prompt => prompt.id === promptID)!, version: undefined, output: undefined }
   }
 
   const addPrompt = (promptID: number) => {
@@ -42,25 +44,41 @@ export default function BuildChainTab({ prompts }: { prompts: Prompt[] }) {
     setChain([...chain.slice(0, index), ...chain.slice(index + 1)])
   }
 
+  const asLoaded = (item: ChainItem) => item as { prompt: ActivePrompt; version: Version; output?: string }
+
   const selectVersion = (index: number) => (version: Version) => {
+    setChain([...chain.slice(0, index), { ...asLoaded(chain[index]), version }, ...chain.slice(index + 1)])
+  }
+
+  const mapOutput = (index: number) => (output?: string) => {
+    const resetChain = chain.map(item =>
+      item.version ? { ...item, output: item.output === output ? undefined : item.output } : item
+    )
     setChain([
-      ...chain.slice(0, index),
-      { prompt: chain[index].prompt as ActivePrompt, version },
-      ...chain.slice(index + 1),
+      ...resetChain.slice(0, index),
+      { ...asLoaded(resetChain[index]), output },
+      ...resetChain.slice(index + 1),
     ])
   }
 
-  const allInputVariables = [... new Set(chain.flatMap(item => item.version ? ExtractPromptVariables(item.version.prompt) : []))]
+  const variablesFromItem = (item: ChainItem) => (item.version ? ExtractPromptVariables(item.version.prompt) : [])
+  const variablesFromSlice = (slice: ChainItem[]) => [...new Set(slice.flatMap(variablesFromItem))]
+  const variablesFromIndex = (index: number) => variablesFromSlice(chain.slice(index + 1))
+  const allInputVariables = variablesFromSlice(chain)
+  const boundInputVariables = chain.map(item => item.output).filter(output => !!output) as string[]
+  const unboundInputVariables = allInputVariables.filter(variable => !boundInputVariables.includes(variable))
 
   return (
     <>
-      <div className='flex flex-col flex-grow h-full gap-4 p-6 max-w-[50%]'>
+      <div className='flex flex-col flex-grow h-full gap-4 p-6'>
         <div className='flex flex-wrap gap-2'>
           <Label>Inputs:</Label>
-          {allInputVariables.map((variable, index) => (
-            <span key={index} className='text-white rounded px-1 py-0.5 bg-violet-500 font-normal'>{variable}</span>
+          {unboundInputVariables.map((variable, index) => (
+            <span key={index} className='text-white rounded px-1 py-0.5 bg-violet-500 font-normal'>
+              {variable}
+            </span>
           ))}
-          </div>
+        </div>
         {chain.map((item, index) => (
           <div key={index} className='flex gap-4'>
             <PromptSelector
@@ -70,11 +88,21 @@ export default function BuildChainTab({ prompts }: { prompts: Prompt[] }) {
               onRemovePrompt={removePrompt(index)}
             />
             {item.version && (
-              <VersionSelector
-                versions={item.prompt.versions}
-                endpoints={item.prompt.endpoints}
-                activeVersion={item.version}
-                setActiveVersion={selectVersion(index)}
+              <Selector>
+                <VersionSelector
+                  versions={item.prompt.versions}
+                  endpoints={item.prompt.endpoints}
+                  activeVersion={item.version}
+                  setActiveVersion={selectVersion(index)}
+                />
+              </Selector>
+            )}
+            {item.version && variablesFromIndex(index).length > 0 && (
+              <OutputMapper
+                key={item.output}
+                output={item.output}
+                inputs={variablesFromIndex(index)}
+                onMapOutput={mapOutput(index)}
               />
             )}
           </div>
@@ -87,6 +115,33 @@ export default function BuildChainTab({ prompts }: { prompts: Prompt[] }) {
         />
       </div>
     </>
+  )
+}
+
+function Selector({ children }: { children: ReactNode }) {
+  return <div className='w-full max-w-[30%]'>{children}</div>
+}
+
+function OutputMapper({
+  output,
+  inputs,
+  onMapOutput,
+}: {
+  output?: string
+  inputs: string[]
+  onMapOutput: (input?: string) => void
+}) {
+  return (
+    <Selector>
+      <DropdownMenu value={output ?? 0} onChange={value => onMapOutput(Number(value) === 0 ? undefined : value)}>
+        <option value={0}>Map Output</option>
+        {inputs.map((input, index) => (
+          <option key={index} value={input}>
+            {input}
+          </option>
+        ))}
+      </DropdownMenu>
+    </Selector>
   )
 }
 
@@ -104,17 +159,19 @@ function PromptSelector({
   onRemovePrompt?: () => void
 }) {
   return (
-    <DropdownMenu
-      value={selectedPrompt?.id}
-      onChange={value => (onRemovePrompt && !Number(value) ? onRemovePrompt() : onSelectPrompt(Number(value)))}>
-      {includeAddPromptOption && <option value={0}>Add Prompt to Chain</option>}
-      {prompts.map((prompt, index) => (
-        <option key={index} value={prompt.id}>
-          {prompt.name}
-        </option>
-      ))}
-      {onRemovePrompt && <option disabled>────────────────────────────────────</option>}
-      {onRemovePrompt && <option value={0}>Remove Prompt from Chain</option>}
-    </DropdownMenu>
+    <Selector>
+      <DropdownMenu
+        value={selectedPrompt?.id}
+        onChange={value => (onRemovePrompt && !Number(value) ? onRemovePrompt() : onSelectPrompt(Number(value)))}>
+        {includeAddPromptOption && <option value={0}>Add Prompt to Chain</option>}
+        {prompts.map((prompt, index) => (
+          <option key={index} value={prompt.id}>
+            {prompt.name}
+          </option>
+        ))}
+        {onRemovePrompt && <option disabled>────────────────────────────────────</option>}
+        {onRemovePrompt && <option value={0}>Remove Prompt from Chain</option>}
+      </DropdownMenu>
+    </Selector>
   )
 }
