5d4494f3ee5befd528697bbcb80d226989cfbdc1
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-07-04 11:51:00 +0100

    Factor out chain evaluation logic

--- pages/api/runChain.ts
+++ pages/api/runChain.ts
@@ -4,6 +4,27 @@ import { saveRun } from '@/src/server/datastore/runs'
 import { PromptInputs, PromptConfig, User, Run } from '@/types'
 import { runPromptWithConfig } from './runPrompt'
 
+export const runChainWithInputs = async (
+  userID: number,
+  chain: { promptID: number; versionID: number; prompt: string; config: PromptConfig; mappedOutput?: string }[],
+  multipleInputs: PromptInputs[]
+): Promise<Run[]> => {
+  const runs: Run[] = []
+  for (const inputs of multipleInputs) {
+    for (const item of chain) {
+      const { output, cost } = await runPromptWithConfig(item.prompt, item.config, inputs, false)
+      if (!output?.length) {
+        break
+      }
+      runs.push(await saveRun(userID, item.promptID, item.versionID, inputs, output, cost))
+      if (item.mappedOutput) {
+        inputs[item.mappedOutput] = output
+      }
+    }
+  }
+  return runs
+}
+
 async function runChain(req: NextApiRequest, res: NextApiResponse<Run[]>, user: User) {
   const promptIDs: number[] = req.body.promptIDs
   const configs: PromptConfig[] = req.body.configs
@@ -12,23 +33,18 @@ async function runChain(req: NextApiRequest, res: NextApiResponse<Run[]>, user: 
   const multipleInputs: PromptInputs[] = req.body.inputs
   const mappedOutputs: (string | undefined)[] = req.body.outputs
 
-  const runs: Run[] = []
-  for (const inputs of multipleInputs) {
-    for (const [index, versionID] of versionIDs.entries()) {
-      const promptID = promptIDs[index]
-      const prompt = prompts[index]
-      const config = configs[index]
-      const { output, cost } = await runPromptWithConfig(prompt, config, inputs, false)
-      if (!output?.length) {
-        break
-      }
-      runs.push(await saveRun(user.id, promptID, versionID, inputs, output, cost))
-      const mappedOutput = mappedOutputs[index]
-      if (mappedOutput) {
-        inputs[mappedOutput] = output
-      }
-    }
-  }
+  const runs = await runChainWithInputs(
+    user.id,
+    promptIDs.map((promptID, index) => ({
+      promptID,
+      versionID: versionIDs[index],
+      prompt: prompts[index],
+      config: configs[index],
+      mappedOutput: mappedOutputs[index],
+    })),
+    multipleInputs
+  )
+
   res.json(runs)
 }
 
