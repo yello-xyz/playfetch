2723e2bfe97cbc64da737aa270feae17bcfe9a86
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-07-10 18:58:01 +0100

    Fix build warning

--- components/useContainerRect.tsx
+++ components/useContainerRect.tsx
@@ -1,10 +1,10 @@
 import useResizeObserver from '@react-hook/resize-observer'
-import { RefObject, useLayoutEffect, useState } from 'react'
+import { RefObject, useEffect, useState } from 'react'
 
 export default function useContainerRect(target: RefObject<HTMLElement>) {
   const [rect, setRect] = useState<DOMRect>()
 
-  useLayoutEffect(() => setRect(target.current?.getBoundingClientRect()), [target])
+  useEffect(() => setRect(target.current?.getBoundingClientRect()), [target])
   useResizeObserver(target, _ => setRect(target.current?.getBoundingClientRect()))
 
   return rect
