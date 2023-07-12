e0fb0b3e4a75e4eebe35ea8c9c9a01f16e33f5bf
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-07-12 17:59:48 +0100

    Try some stuff to make streaming work on cloud

--- pages/api/stream.ts
+++ pages/api/stream.ts
@@ -2,16 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 async function stream(_: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Content-Type', 'text/event-stream;charset=utf-8')
-  res.setHeader('Cache-Control', 'no-cache, no-transform')
+  res.setHeader('Content-Type', 'text/event-stream')
+  res.setHeader('Cache-Control', 'no-store"')
   res.setHeader('X-Accel-Buffering', 'no')
   res.setHeader('Content-Encoding', 'none')
 
-  res.write('first chunk')
+  res.status(202).write('first chunk')
   await new Promise(resolve => setTimeout(resolve, 3000))
-  res.write('second chunk with newline\n')
+  res.status(202).write('second chunk with newline\n')
   await new Promise(resolve => setTimeout(resolve, 3000))
-  res.write('last chunk')
+  res.status(202).write('last chunk')
   await new Promise(resolve => setTimeout(resolve, 3000))
 
   res.end()
