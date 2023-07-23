08fb2bda7b1083898386d0531b77bf1bb25e8429
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-07-23 10:25:52 +0100

    Don't need to repeat output here
    I think it was previously needed only to assure the typing, but now we allow an undefined output to be included (since we also callback on failure).

--- pages/api/runChain.ts
+++ pages/api/runChain.ts
@@ -71,7 +71,7 @@ export const runChainConfigs = async (
       runningContext += `\n\n${output}\n\n`
       AugmentInputs(inputs, config.output, output, useCamelCase)
       AugmentCodeContext(codeContext, config.output, result)
-      await callback(index, version, { ...runResponse, output, failed: false })
+      await callback(index, version, { ...runResponse, failed: false })
     } else {
       const codeResponse = await EvaluateCode(config.code, codeContext)
       result = codeResponse.result
