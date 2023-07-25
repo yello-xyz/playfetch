c16a637c0049db694e6d0216d76c282df756ea14
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-07-25 10:31:10 +0100

    Ensure different types of buttons have same height

--- components/button.tsx
+++ components/button.tsx
@@ -43,17 +43,17 @@ export default function Button({
     switch (type) {
       default:
       case 'primary':
-        return 'text-white bg-blue-600 hover:bg-blue-800'
+        return 'text-white border-blue-600 bg-blue-600 hover:bg-blue-800'
       case 'outline':
-        return 'bg-gray-200 hover:bg-gray-300'
+        return 'bg-gray-200 border-gray-200 hover:bg-gray-300'
       case 'destructive':
-        return 'text-red-500 border border-gray-200 hover:bg-gray-100'
+        return 'text-red-500 border-gray-200 hover:bg-gray-100'
     }
   }
 
   return (
     <button
-      className={`${colorForType(type)} text-sm whitespace-nowrap px-4 py-2 rounded-lg disabled:opacity-50`}
+      className={`${colorForType(type)} text-sm border whitespace-nowrap px-4 py-2 rounded-lg disabled:opacity-50`}
       disabled={disabled}
       onClick={onClick}>
       {children}
