cc234e9da7fdd22fa674dbda6ae927cd2015a225
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-07-27 06:09:48 +0100

    Redirect to https in middleware
    Apparently flexible app engine does not current support handlers in the app.yaml.

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { GetSecureURL, IsLocalHost, IsSecure } from './src/server/routing'
+
+export function middleware(request: NextRequest) {
+  const headers = new Headers(request.headers)
+  const getHeader = (key: string) => headers.get(key)
+
+  if (!IsSecure(getHeader) && !IsLocalHost(getHeader)) {
+    console.log('redirecting', GetSecureURL(getHeader).href)
+    return NextResponse.redirect(GetSecureURL(getHeader).href, 301)
+  } else {
+    console.log('not redirecting')
+    return NextResponse.next()
+  }
+}
