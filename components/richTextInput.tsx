488fba9d6c81fc40f071fd0be98646b9105f612a
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-07-27 11:46:09 +0200

    Reimplement rich text logic
    Should be less fragile now and easier to add tests for additional edge cases.

--- components/richTextInput.tsx
+++ components/richTextInput.tsx
@@ -11,29 +11,56 @@ const ContentEditable = dynamic(() => import('./contentEditable'))
 const LinkedVariableClass = `${InputVariableClass} pl-5 bg-no-repeat bg-[left_2px_center]`
 const LinkedVariableStyle = `background-image: url('${linkIcon.src}')`
 
-export const RichTextToHTML = (text: string) =>
+const escapeSpecialCharacters = (text: string) =>
+  text.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;')
+
+const unescapeSpecialCharacters = (html: string) =>
+  html.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+
+const printVariables = (text: string) =>
   text
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
     .replace(/}}$/, '}}&nbsp;')
     .replace(/{{([^{]*?)}}/g, `<b class="${LinkedVariableClass}" style="${LinkedVariableStyle}">$1</b>`)
-    .replace(/\n(.*)$/gm, '<div>$1</div>')
-    .replaceAll('<div></div>', '<div><br /></div>')
 
-export const RichTextFromHTML = (html: string) =>
+const parseVariables = (html: string) =>
   html
-    .replace(/}}&nbsp;$/, '}}')
-    .replaceAll('<br />', '')
-    .replace(/<div>(.*?)<\/div>/g, '\n$1')
     .replace(/<b[^>]*>(.*?)<\/b>/g, '{{$1}}')
+    .replace(/}}&nbsp;$/, '}}')
     .replaceAll('{{}}', '')
-    .replace(/[\u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g, ' ')
     .replace(/{{(.*?)([ \.]+)}}([^ ])/g, '{{$1}}$2$3')
     .replace(/([^ ]){{([ \.]+)(.*?)}}/g, '$1$2{{$3}}')
-    .replace(/&amp;/g, '&')
-    .replace(/&lt;/g, '<')
-    .replace(/&gt;/g, '>')
+
+const linesToDivs = (text: string) =>
+  text
+    .split('\n')
+    .map((line, index, lines) =>
+      line.length === 0 && lines.length > 1 ? `<div><br /></div>` : index === 0 ? line : `<div>${line}<br /></div>`
+    )
+    .join('')
+
+const flattenDivs = (html: string) =>
+  html
+    .replaceAll('<div></div>', '')
+    .replace(/<div><div>(.*)<\/div><\/div>/g, '<div>$1</div>')
+    .replace(/<div><div>(.*)<\/div>(.*)<\/div>/g, '<div>$1</div><div>$2</div>')
+    .replace(/<div>(.*)<div>(.*)<\/div><\/div>/g, '<div>$1</div><div>$2</div>')
+    .replaceAll('<br />', '')
+    .replace(/[\u00A0\u1680​\u180e\u2000-\u2009\u200a​\u200b​\u202f\u205f​\u3000]/g, ' ')
+
+const divsToLines = (html: string) => {
+  const flattened = flattenDivs(html)
+  const matches = [...flattened.matchAll(/(.*?)<div>(.*?)<\/div>/g)]
+  const divs = matches.map(match => match[0]).join('')
+  const end = flattened.indexOf(divs) + divs.length
+  return [
+    ...matches.flatMap(match => (match[1].length ? [match[1], match[2]] : [match[2]])),
+    ...(end <= flattened.length - 1 ? [flattened.substring(end)] : []),
+  ].join('\n')
+}
+
+export const RichTextToHTML = (text: string) => linesToDivs(printVariables(escapeSpecialCharacters(text)))
+
+export const RichTextFromHTML = (html: string) => unescapeSpecialCharacters(parseVariables(divsToLines(html)))
 
 type Selection = { text: string; range: Range; popupPoint: { x: number; y: number }; isInput: boolean }
 
@@ -109,13 +136,24 @@ export default function RichTextInput({
     ? 'outline-none'
     : 'p-4 overflow-y-auto text-gray-800 border border-gray-300 rounded-lg'
 
+  const [htmlValue, setHTMLValue] = useState('')
+  if (value !== RichTextFromHTML(htmlValue)) {
+    setHTMLValue(RichTextToHTML(value))
+  } else if (printVariables(parseVariables(htmlValue)) !== htmlValue) {
+    setHTMLValue(printVariables(parseVariables(htmlValue)))
+  }
+  const updateHTMLValue = (html: string) => {
+    setHTMLValue(html)
+    setValue(RichTextFromHTML(html))
+  }
+
   const renderContentEditable = () => (
     <Suspense>
       <ContentEditable
         disabled={disabled}
         className={contentEditableClassName}
-        htmlValue={RichTextToHTML(value)}
-        onChange={value => setValue(RichTextFromHTML(value))}
+        htmlValue={htmlValue}
+        onChange={updateHTMLValue}
         allowedTags={['br', 'div', 'b']}
         allowedAttributes={{ b: ['class'] }}
         innerRef={contentEditableRef}
