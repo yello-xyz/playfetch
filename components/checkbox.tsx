6dd74a3092e0ec4cac43340501f9b8f258fe6501
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-08-12 11:38:15 +0200

    Add disabled states to checkbox component

--- components/checkbox.tsx
+++ components/checkbox.tsx
@@ -1,6 +1,8 @@
 import Label from './label'
 import toggleOn from '@/public/toggleOn.svg'
 import toggleOff from '@/public/toggleOff.svg'
+import toggleOnDisabled from '@/public/toggleOnDisabled.svg'
+import toggleOffDisabled from '@/public/toggleOffDisabled.svg'
 
 export default function Checkbox({
   label,
@@ -13,19 +15,20 @@ export default function Checkbox({
   id?: string
   disabled?: boolean
   checked: boolean
-  setChecked: (checked: boolean) => void
+  setChecked?: (checked: boolean) => void
 }) {
+  const backgroundImage = disabled ? (checked ? toggleOnDisabled : toggleOffDisabled) : checked ? toggleOn : toggleOff
   return (
     <div className='flex items-center justify-between gap-2'>
       {label && <Label htmlFor={id}>{label}</Label>}
       <input
         className={`w-[42px] h-6 ${disabled ? '' : 'cursor-pointer'} appearance-none`}
-        style={{ backgroundImage: `url('${checked ? toggleOn.src : toggleOff.src}')` }}
+        style={{ backgroundImage: `url('${backgroundImage.src}')` }}
         type='checkbox'
         id={id}
         disabled={disabled}
         checked={checked}
-        onChange={event => setChecked(event.target.checked)}
+        onChange={event => setChecked?.(event.target.checked)}
       />
     </div>
   )
