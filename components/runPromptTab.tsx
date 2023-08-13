be8c997511dc7733365daa42449c406a8f4c6937
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-08-13 11:22:50 +0200

    Fix broken padding in version timeline
    Looks like this is in progress of being changed but let's avoid it looking broken for now.

--- components/runPromptTab.tsx
+++ components/runPromptTab.tsx
@@ -31,7 +31,7 @@ export default function RunPromptTab({
   return (
     <Allotment vertical>
       <Allotment.Pane minSize={minHeight} preferredSize='50%'>
-        <div className='h-full'>
+        <div className='h-full p-4'>
           <VersionTimeline
             prompt={activePrompt}
             activeVersion={activeVersion}
