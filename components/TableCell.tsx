a13c91fde1bf1e273e949f6dc140f1264078a6d4
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-08-21 10:11:11 +0100

    Fix selection colour for table cells

--- components/TableCell.tsx
+++ components/TableCell.tsx
@@ -17,7 +17,7 @@ export default function TableCell({
   active?: boolean
   callback?: () => void
 }) {
-  const baseClass = 'px-3 py-2 text-ellipsis overflow-hidden border-gray-200 flex bg-white'
+  const baseClass = 'px-3 py-2 text-ellipsis overflow-hidden border-gray-200 flex'
   const borderClass = header
     ? first
       ? 'border-y border-l'
@@ -30,7 +30,7 @@ export default function TableCell({
     ? 'border-b border-r'
     : 'border-b'
   const textClass = header ? 'font-medium text-gray-700' : ''
-  const bgClass = active ? 'bg-blue-25' : ''
+  const bgClass = active ? 'bg-blue-25' : 'bg-white'
   const layoutClass = center ? 'flex justify-center' : ''
   const cursorClass = callback ? 'cursor-pointer' : ''
   const className = `${baseClass} ${borderClass} ${textClass} ${bgClass} ${layoutClass} ${cursorClass}`
