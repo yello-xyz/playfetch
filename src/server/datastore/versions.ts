72ce32b89a567fae3d4e6e686508680c79eec2cf
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-08-29 09:26:11 +0100

    Delete cache entries when deleting version

--- src/server/datastore/versions.ts
+++ src/server/datastore/versions.ts
@@ -269,9 +269,10 @@ export async function deleteVersionForUser(userID: number, versionID: number) {
   const wasLastVersion = versionCount === 1
   const wasPromptVersion = IsPromptVersion(versionData)
 
+  const cacheKeys = await getEntityKeys(Entity.CACHE, 'versionID', versionID)
   const runKeys = await getEntityKeys(Entity.RUN, 'versionID', versionID)
   const commentKeys = await getEntityKeys(Entity.COMMENT, 'versionID', versionID)
-  await getDatastore().delete([...commentKeys, ...runKeys, buildKey(Entity.VERSION, versionID)])
+  await getDatastore().delete([...cacheKeys, ...commentKeys, ...runKeys, buildKey(Entity.VERSION, versionID)])
 
   if (wasLastVersion) {
     if (wasPromptVersion) {
