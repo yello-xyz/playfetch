11090f40a235fedd5c9177966a5841916c58f000
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-08-31 17:44:49 +0100

    Factor out LabelsPopup from Menu component

--- components/labelPopupMenu.tsx
+++ components/labelPopupMenu.tsx
@@ -43,21 +43,50 @@ export default function LabelPopupMenu({
   containerRect?: DOMRect
 }) {
   const [isMenuExpanded, setMenuExpanded] = useState(false)
+
+  const iconRef = useRef<HTMLDivElement>(null)
+  const [popupRect, popupRef] = useClientRect()
+
+  return (
+    <>
+      <div ref={iconRef}>
+        <IconButton icon={labelIcon} onClick={() => setMenuExpanded(!isMenuExpanded)} />
+      </div>
+      {isMenuExpanded && (
+        <div className='absolute' style={CalculatePopupOffset(iconRef, containerRect, popupRect)}>
+          <PopupMenu expanded={isMenuExpanded} collapse={() => setMenuExpanded(false)}>
+            <div ref={popupRef} className='p-3 w-80'>
+              <LabelsPopup item={item} activeItem={activeItem} onDismiss={() => setMenuExpanded(false)} />
+            </div>
+          </PopupMenu>
+        </div>
+      )}
+    </>
+  )
+}
+
+function LabelsPopup({
+  item,
+  activeItem,
+  onDismiss,
+}: {
+  item: PromptVersion | ChainVersion | Run
+  activeItem: ActivePrompt | ActiveChain
+  onDismiss: () => void
+}) {
   const [newLabel, setNewLabel] = useState('')
   const trimmedLabel = newLabel.trim()
 
   const labels = activeItem.availableLabels
+
   const colors = AvailableLabelColorsForItem(activeItem)
 
   const addingNewLabel = trimmedLabel.length > 0 && !labels.includes(trimmedLabel)
 
   const refreshActiveItem = useRefreshActiveItem()
 
-  const iconRef = useRef<HTMLDivElement>(null)
-  const [popupRect, popupRef] = useClientRect()
-
   const toggleLabel = (label: string) => {
-    setMenuExpanded(false)
+    onDismiss()
     setNewLabel('')
     const checked = !item.labels.includes(label)
     const itemIsVersion = 'runs' in item
@@ -70,40 +99,29 @@ export default function LabelPopupMenu({
 
   return (
     <>
-      <div ref={iconRef}>
-        <IconButton icon={labelIcon} onClick={() => setMenuExpanded(!isMenuExpanded)} />
-      </div>
-      {isMenuExpanded && (
-        <div className='absolute' style={CalculatePopupOffset(iconRef, containerRect, popupRect)}>
-          <PopupMenu expanded={isMenuExpanded} collapse={() => setMenuExpanded(false)}>
-            <div ref={popupRef} className='p-3 w-80'>
-              <input
-                type='text'
-                className='w-full text-sm mb-3 border border-gray-300 outline-none rounded-lg px-3 py-1.5'
-                placeholder='Add a label'
-                value={newLabel}
-                onChange={event => setNewLabel(event.target.value)}
-              />
-              {addingNewLabel ? (
-                <div className='flex items-center gap-1 p-1 cursor-pointer' onClick={() => toggleLabel(trimmedLabel)}>
-                  <Icon icon={addIcon} />
-                  Create new label <span className='font-medium'>“{trimmedLabel}”</span>
-                </div>
-              ) : (
-                labels.map((label, index) => (
-                  <div
-                    className='flex items-center gap-1 px-2 py-1 cursor-pointer'
-                    key={index}
-                    onClick={() => toggleLabel(label)}>
-                    <div className={`w-2.5 h-2.5 m-2.5 rounded-full ${colors[label]}`} />
-                    <div className='flex-1'>{label}</div>
-                    {item.labels.includes(label) && <Icon icon={checkIcon} />}
-                  </div>
-                ))
-              )}
-            </div>
-          </PopupMenu>
+      <input
+        type='text'
+        className='w-full text-sm mb-3 border border-gray-300 outline-none rounded-lg px-3 py-1.5'
+        placeholder='Add a label'
+        value={newLabel}
+        onChange={event => setNewLabel(event.target.value)}
+      />
+      {addingNewLabel ? (
+        <div className='flex items-center gap-1 p-1 cursor-pointer' onClick={() => toggleLabel(trimmedLabel)}>
+          <Icon icon={addIcon} />
+          Create new label <span className='font-medium'>“{trimmedLabel}”</span>
         </div>
+      ) : (
+        labels.map((label, index) => (
+          <div
+            className='flex items-center gap-1 px-2 py-1 cursor-pointer'
+            key={index}
+            onClick={() => toggleLabel(label)}>
+            <div className={`w-2.5 h-2.5 m-2.5 rounded-full ${colors[label]}`} />
+            <div className='flex-1'>{label}</div>
+            {item.labels.includes(label) && <Icon icon={checkIcon} />}
+          </div>
+        ))
       )}
     </>
   )
