yello-xyz/playfetch

commit 7148377f28b8b47c545912ca0cc6d9880b20c895

Refresh sidebar on saving prompt
Prompt name might update as part of saving prompt so we should refresh the project as well.

pages/[projectID]/index.tsx
@@ -253,7 +253,9 @@ export default function Home({
                         setModifiedVersion={setModifiedVersion}
                         showComments={showComments}
                         setShowComments={setShowComments}
-                        savePrompt={() => savePrompt(refreshActivePrompt).then(versionID => versionID!)}
+                        savePrompt={() =>
+                          savePrompt(_ => Promise.resolve(refreshActiveItem())).then(versionID => versionID!)
+                        }
                       />
                     </Suspense>
                   )}

commit a368fd06218b875e6fce6be6dbe86e9c26cefbf9

Fix refresh version on saving prompt
When saving the prompt creates a new version, we should select that new version (otherwise we risk still viewing old responses while running the new version). This bug was introduced with
https://github.com/yello-xyz/playfetch/commit/7148377f28b8b47c545912ca0cc6d9880b20c895

pages/[projectID]/index.tsx
@@ -158,8 +158,8 @@ export default function Home({
     }
   }
 
-  const refresh = () => {
-    refreshActiveItem()
+  const refresh = (versionID?: number) => {
+    refreshActiveItem(versionID)
     if (activeItem !== Endpoints) {
       refreshProject()
     }
@@ -239,7 +239,7 @@ export default function Home({
                     onAddPrompt={addPrompt}
                     onAddChain={addChain}
                     onDeleteItem={onDeleteItem}
-                    onRefreshItem={refresh}
+                    onRefreshItem={() => refresh()}
                     onSelectPrompt={selectPrompt}
                     onSelectChain={selectChain}
                     onSelectEndpoints={selectEndpoints}