48809b0e4242b3741d4bb7f768269573dd3fbc0d
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-09-01 12:28:28 +0100

    Fix edge case in selection range calculation
    We can't assume that the starts in the anchorNode because the selection could go from right to left (in which case we should treat the focusNode as the start element).

--- components/runCell.tsx
+++ components/runCell.tsx
@@ -13,25 +13,38 @@ type Selection = { text: string; startIndex: number; popupPoint: { x: number; y:
 
 const extractSelection = (identifier: string) => {
   const selection = document.getSelection()
-  if (selection) {
-    const selectionElement = selection?.anchorNode?.parentElement
-    const containerElement = selectionElement?.parentElement
-    const endContainer = selection?.focusNode?.parentElement?.parentElement
-    const text = selection.toString().trim()
-    if (containerElement?.id === identifier && endContainer?.id === identifier && selectionElement && text.length > 0) {
-      const spans = [...containerElement.children]
-      const precedingSpans = spans.slice(0, spans.indexOf(selectionElement))
-      const spanOffset = precedingSpans.reduce((len, node) => len + (node.textContent?.length ?? 0), 0)
-      const range = selection.getRangeAt(0)
-      const selectionRect = range.getBoundingClientRect()
-      const popupPoint = {
-        x: selectionRect.left + selectionRect.width / 2,
-        y: selectionRect.top - 42,
-      }
-      return { text, popupPoint, startIndex: range.startOffset + spanOffset }
-    }
+  if (!selection) {
+    return undefined
   }
-  return undefined
+
+  const text = selection.toString().trim()
+  const selectionStartElement = selection?.anchorNode?.parentElement
+  const selectionEndElement = selection?.focusNode?.parentElement
+  if (text.length === 0 || !selectionStartElement || !selectionEndElement) {
+    return undefined
+  }
+
+  const startContainer = selectionStartElement.parentElement
+  const endContainer = selectionEndElement.parentElement
+  if (startContainer?.id !== identifier || endContainer?.id !== identifier) {
+    return undefined
+  }
+
+  const spans = [...startContainer.children]
+  const startElementIndex = spans.indexOf(selectionStartElement)
+  const endElementIndex = spans.indexOf(selectionEndElement)
+  const selectionElement = startElementIndex <= endElementIndex ? selectionStartElement : selectionEndElement
+  const precedingSpans = spans.slice(0, spans.indexOf(selectionElement))
+  const spanOffset = precedingSpans.reduce((len, node) => len + (node.textContent?.length ?? 0), 0)
+  const range = selection.getRangeAt(0)
+  
+  const selectionRect = range.getBoundingClientRect()
+  const popupPoint = {
+    x: selectionRect.left + selectionRect.width / 2,
+    y: selectionRect.top - 42,
+  }
+  
+  return { text, popupPoint, startIndex: range.startOffset + spanOffset }
 }
 
 export default function RunCell({
