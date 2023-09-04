905573c4a54621742342da0477eeba8aa553a030
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-09-04 15:56:14 +0100

    Factor out Collapsible component

--- components/promptSettingsPane.tsx
+++ components/promptSettingsPane.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { PromptConfig } from '@/types'
 import chevronIcon from '@/public/chevron.svg'
 import Icon from './icon'
+import Collapsible from './collapsible'
 
 const SettingsRow = ({ label, children }: { label: string; children: React.ReactNode }) => (
   <div className='flex items-center w-full gap-4'>
@@ -25,45 +26,37 @@ export default function PromptSettingsPane({
   const updateMaxTokens = (maxTokens: number) => !isNaN(maxTokens) && setConfig({ ...config, maxTokens })
 
   return (
-    <>
-      <div
-        className='flex items-center font-medium text-gray-600 cursor-pointer'
-        onClick={() => setExpanded(!isExpanded)}>
-        <Icon className={`-ml-1 ${isExpanded ? '' : '-rotate-90'}`} icon={chevronIcon} />
-        Advanced Settings
+    <Collapsible label='Advanced Settings' isExpanded={isExpanded} setExpanded={setExpanded}>
+      <div className='flex flex-col gap-2 px-6 py-4 -mt-1 border border-gray-200 border-solid rounded-lg bg-gray-25'>
+        <SettingsRow label='Temperature'>
+          <input
+            className='w-1/3 border border-gray-300 accent-gray-700 focus:border focus:border-blue-400 focus:ring-0 focus:outline-none'
+            type='range'
+            min={0}
+            max={1}
+            step={0.01}
+            value={config.temperature}
+            onChange={event => updateTemperature(Number(event.target.value))}
+          />
+          <input
+            className='p-2 text-sm border border-gray-300 rounded-lg w-18 focus:border-solid focus:border focus:border-blue-400 focus:ring-0 focus:outline-none'
+            type='number'
+            min={0}
+            max={1}
+            step={0.01}
+            value={config.temperature}
+            onChange={event => updateTemperature(Number(event.target.value))}
+          />
+        </SettingsRow>
+        <SettingsRow label='Maximum tokens'>
+          <input
+            className='w-20 p-2 text-sm border border-gray-300 rounded-lg focus:border-blue-400 focus:border focus:ring-0 focus:outline-none'
+            type='text'
+            value={config.maxTokens}
+            onChange={event => updateMaxTokens(Number(event.target.value))}
+          />
+        </SettingsRow>
       </div>
-      {isExpanded && (
-        <div className='flex flex-col gap-2 px-6 py-4 -mt-1 border border-gray-200 border-solid rounded-lg bg-gray-25'>
-          <SettingsRow label='Temperature'>
-            <input
-              className='w-1/3 border border-gray-300 accent-gray-700 focus:border focus:border-blue-400 focus:ring-0 focus:outline-none'
-              type='range'
-              min={0}
-              max={1}
-              step={0.01}
-              value={config.temperature}
-              onChange={event => updateTemperature(Number(event.target.value))}
-            />
-            <input
-              className='p-2 text-sm border border-gray-300 rounded-lg w-18 focus:border-solid focus:border focus:border-blue-400 focus:ring-0 focus:outline-none'
-              type='number'
-              min={0}
-              max={1}
-              step={0.01}
-              value={config.temperature}
-              onChange={event => updateTemperature(Number(event.target.value))}
-            />
-          </SettingsRow>
-          <SettingsRow label='Maximum tokens'>
-            <input
-              className='w-20 p-2 text-sm border border-gray-300 rounded-lg focus:border-blue-400 focus:border focus:ring-0 focus:outline-none'
-              type='text'
-              value={config.maxTokens}
-              onChange={event => updateMaxTokens(Number(event.target.value))}
-            />
-          </SettingsRow>
-        </div>
-      )}
-    </>
+    </Collapsible>
   )
 }
