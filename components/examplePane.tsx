fcbc7dec20b4f8316af4cce365e524ea03fbf7c1
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-09-04 12:02:11 +0100

    Allow to scroll code block if needed
    We don't want to show the line number overflow, but we may still need to scroll the container if there is a lot of content.

--- components/examplePane.tsx
+++ components/examplePane.tsx
@@ -63,10 +63,12 @@ export default function ExamplePane({
 }
 
 export function CodeBlock({ children, active, error }: { children: ReactNode; active?: boolean; error?: boolean }) {
+  const baseClass = 'p-4 text-xs rounded-lg bg-white overflow-y-auto border border-gray-200'
   const textColorClass = error ? 'text-red-300' : 'text-green-300'
   const borderClass = active ? 'border' : ''
   return (
-    <div className={`p-4 text-xs rounded-lg bg-white border border-gray-200 ${textColorClass} ${borderClass}`}>
+    <div
+      className={`${baseClass} ${textColorClass} ${borderClass}`}>
       <div className='relative overflow-hidden'>
         <pre className='pl-10 break-all whitespace-pre-wrap'>{children}</pre>
         <div className='absolute top-0 left-0'>
