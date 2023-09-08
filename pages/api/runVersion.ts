df35b165b0d852ba34039c0934e7442debb74c44
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-09-08 09:58:47 +0100

    Use timestamp to designate intermediate results
    Currently we rely on the indices to determine which partial runs were intermediate results (and hence can stay on screen after completing the run). This is a bit fragile especially if we get additional steps (indices) because of interrupts. It's better to just let the backend mark the runs that should be persisted by adding/removing the timestamp.

--- pages/api/runVersion.ts
+++ pages/api/runVersion.ts
@@ -8,23 +8,25 @@ import runChain from '@/src/server/chainEngine'
 export const loadConfigsFromVersion = (version: RawPromptVersion | RawChainVersion): (RunConfig | CodeConfig)[] =>
   version.items ?? [{ versionID: version.id }]
 
-const logResponse =
-  (userID: number, version: RawPromptVersion | RawChainVersion, inputs: PromptInputs) =>
-  (response: Awaited<ReturnType<typeof runChain>>) => {
-    if (!response.failed) {
-      saveRun(
-        userID,
-        version.parentID,
-        version.id,
-        inputs,
-        response.output,
-        new Date(),
-        response.cost,
-        response.duration,
-        []
-      )
-    }
-  }
+const logResponse = (
+  userID: number,
+  version: RawPromptVersion | RawChainVersion,
+  inputs: PromptInputs,
+  response: Awaited<ReturnType<typeof runChain>> & { output: string }
+) =>
+  saveRun(
+    userID,
+    version.parentID,
+    version.id,
+    inputs,
+    response.output,
+    new Date(),
+    response.cost,
+    response.duration,
+    []
+  )
+
+const timestampIf = (condition: boolean) => (condition ? new Date().toISOString() : undefined)
 
 async function runVersion(req: NextApiRequest, res: NextApiResponse, user: User) {
   const versionID = req.body.versionID
@@ -39,10 +41,21 @@ async function runVersion(req: NextApiRequest, res: NextApiResponse, user: User)
   await Promise.all(
     multipleInputs.map(async (inputs, inputIndex) => {
       const offset = (index: number) => inputIndex * configs.length + index
-      const timestamp = (failed?: boolean) => (failed !== undefined ? new Date().toISOString() : undefined)
       return runChain(user.id, version, configs, inputs, false, (index, message, cost, duration, failed) =>
-        sendData({ index: offset(index), message, timestamp: timestamp(failed), cost, duration, failed })
-      ).then(logResponse(user.id, version, inputs))
+        sendData({
+          index: offset(index),
+          message,
+          timestamp: timestampIf(failed !== undefined),
+          cost,
+          duration,
+          failed,
+        })
+      ).then(response => {
+        if (!response.failed) {
+          sendData({ index: offset(configs.length - 1) })
+          logResponse(user.id, version, inputs, response)
+        }
+      })
     })
   )
 
