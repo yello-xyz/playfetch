b122271aff45cb5991877e92d36fcad8ab8d117e
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-09-19 16:18:49 +0100

    Move output mapper into chain node box component

--- components/chains/chainNodeEditor.tsx
+++ components/chains/chainNodeEditor.tsx
@@ -1,10 +1,7 @@
 import { ChainItem, PromptVersion } from '@/types'
-import DropdownMenu from '../dropdownMenu'
-import Label from '../label'
 import PromptChainNodeEditor from './promptChainNodeEditor'
 import { IsCodeChainItem, IsPromptChainItem } from './chainNode'
 import CodeChainNodeEditor from './codeChainNodeEditor'
-import { ExtractChainVariables } from './chainNodeOutput'
 import Button, { PendingButton } from '../button'
 import { useState } from 'react'
 import useSavePrompt from '@/src/client/hooks/useSavePrompt'
@@ -30,11 +27,17 @@ export default function ChainNodeEditor({
 }) {
   const [updatedItems, setUpdatedItems] = useInitialState(items, (a, b) => a.length === b.length)
 
-  const updateItems = (items: ChainItem[], item: ChainItem) => [
-    ...items.slice(0, activeIndex),
+  const itemsWithUpdate = (item: ChainItem) => [
+    ...updatedItems.slice(0, activeIndex),
     item,
-    ...items.slice(activeIndex + 1),
+    ...updatedItems.slice(activeIndex + 1),
   ]
+
+  const updateActiveItem = (item: ChainItem) => {
+    const updatedItems = itemsWithUpdate(item)
+    setUpdatedItems(updatedItems)
+    updateItemsDirty(GetChainItemsSaveKey(items) !== GetChainItemsSaveKey(updatedItems))
+  }
 
   const [areItemsDirty, setItemsDirty] = useState(false)
   const [isPromptDirty, setPromptDirty] = useState(false)
@@ -45,12 +48,6 @@ export default function ChainNodeEditor({
   }
   const updateItemsDirty = (itemsDirty: boolean) => updateDirtyState(itemsDirty, isPromptDirty)
   const updatePromptDirty = (promptDirty: boolean) => updateDirtyState(areItemsDirty, promptDirty)
-
-  const updateActiveItem = (item: ChainItem, newItems = updatedItems) => {
-    const updatedItems = updateItems(newItems, item)
-    setUpdatedItems(updatedItems)
-    updateItemsDirty(GetChainItemsSaveKey(items) !== GetChainItemsSaveKey(updatedItems))
-  }
 
   const activeItem = updatedItems[activeIndex] ?? items[activeIndex]
   const isPromptChainItemActive = IsPromptChainItem(activeItem)
@@ -82,16 +79,11 @@ export default function ChainNodeEditor({
     }
   }
 
-  const mapOutput = (output?: string) => {
-    const newItems = updatedItems.map(item => ({ ...item, output: item.output === output ? undefined : item.output }))
-    updateActiveItem({ ...newItems[activeIndex], output }, newItems)
-  }
-
   const colorClass = IsPromptChainItem(activeItem) ? 'bg-white' : 'bg-gray-25'
 
   const saveAndClose = async () => {
     setItems(updatedItems)
-    await saveAndRefreshPrompt(versionID => setItems(updateItems(updatedItems, { ...activeItem, versionID })))
+    await saveAndRefreshPrompt(versionID => setItems(itemsWithUpdate({ ...activeItem, versionID })))
     dismiss()
   }
 
@@ -109,49 +101,13 @@ export default function ChainNodeEditor({
         {IsCodeChainItem(activeItem) && (
           <CodeChainNodeEditor key={activeIndex} item={activeItem} updateItem={updateActiveItem} />
         )}
-        <div className='flex items-center justify-end w-full gap-4 px-4'>
-          {(IsPromptChainItem(activeItem) || IsCodeChainItem(activeItem)) && (
-            <OutputMapper
-              key={activeItem.output}
-              output={activeItem.output}
-              inputs={ExtractChainVariables(items.slice(activeIndex + 1), promptCache, false)}
-              onMapOutput={mapOutput}
-            />
-          )}
-          <div className='flex gap-2'>
-            <Button type='outline' onClick={dismiss}>
-              Cancel
-            </Button>
-            <PendingButton title='Save and close' pendingTitle='Saving' onClick={saveAndClose} />
-          </div>
+        <div className='flex items-center justify-end w-full gap-2 px-4'>
+          <Button type='outline' onClick={dismiss}>
+            Cancel
+          </Button>
+          <PendingButton title='Save and close' pendingTitle='Saving' onClick={saveAndClose} />
         </div>
       </div>
     </>
   )
-}
-
-function OutputMapper({
-  output,
-  inputs,
-  onMapOutput,
-}: {
-  output?: string
-  inputs: string[]
-  onMapOutput: (input?: string) => void
-}) {
-  return inputs.length > 0 ? (
-    <div className='flex-1 self-start py-0.5 flex items-center gap-2'>
-      <Label className='whitespace-nowrap'>Map output to</Label>
-      <DropdownMenu value={output ?? 0} onChange={value => onMapOutput(Number(value) === 0 ? undefined : value)}>
-        <option value={0} disabled>
-          Select Input
-        </option>
-        {inputs.map((input, index) => (
-          <option key={index} value={input}>
-            Input “{input}”
-          </option>
-        ))}
-      </DropdownMenu>
-    </div>
-  ) : null
 }
