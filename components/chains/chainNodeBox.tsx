b11de388cc1fa645510b54e75ef7e63f33b02876
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-09-19 15:11:46 +0100

    Factor out ChainNodeBoxBody component

--- components/chains/chainNodeBox.tsx
+++ components/chains/chainNodeBox.tsx
@@ -1,15 +1,10 @@
-import { ActiveChain, ChainItem, ChainVersion, CodeChainItem, Prompt, PromptChainItem } from '@/types'
-import { ChainNode, InputNode, IsChainItem, IsPromptChainItem } from './chainNode'
+import { ActiveChain, ChainItem, ChainVersion, CodeChainItem, Prompt } from '@/types'
+import { ChainNode } from './chainNode'
 import { PromptCache } from '@/src/client/hooks/usePromptCache'
-import { LabelForModel } from '../prompts/modelSelector'
-import { VersionLabels } from '../versions/versionCell'
-import { AvailableLabelColorsForItem } from '../labelPopupMenu'
 import { ChainNodeBoxConnector } from './chainNodeBoxConnector'
-import { TaggedVersionPrompt } from '../versions/versionComparison'
-import { ReactNode, useState } from 'react'
-import { ExtractUnboundChainVariables } from './chainNodeOutput'
-import { InputVariableClass } from '../prompts/promptInput'
+import { useState } from 'react'
 import { ChainNodeBoxHeader } from './chainNodeBoxHeader'
+import { ChainNodeBoxBody } from './chainNodeBoxBody'
 
 export function ChainNodeBox({
   chain,
@@ -110,66 +105,8 @@ export function ChainNodeBox({
           prompts={prompts}
           users={chain.users}
         />
-        {IsPromptChainItem(chainNode) && (
-          <PromptNodeBody item={chainNode} isSelected={isSelected} promptCache={promptCache} />
-        )}
-        {chainNode === InputNode && <InputNodeBody nodes={nodes} isSelected={isSelected} promptCache={promptCache} />}
+        <ChainNodeBoxBody chainNode={chainNode} nodes={nodes} isSelected={isSelected} promptCache={promptCache} />
       </div>
     </>
   )
-}
-
-function PromptNodeBody({
-  item,
-  isSelected,
-  promptCache,
-}: {
-  item: PromptChainItem
-  isSelected: boolean
-  promptCache: PromptCache
-}) {
-  const prompt = promptCache.promptForItem(item)
-  const version = promptCache.versionForItem(item)
-  const index = prompt?.versions?.findIndex(v => v.id === version?.id) ?? 0
-  return prompt && version ? (
-    <div className='flex flex-col'>
-      <div className='flex flex-col gap-1 pb-3 pl-8 -mt-2.5 ml-0.5'>
-        <span className='text-xs font-medium text-gray-500'>
-          {LabelForModel(version.config.model)} | Prompt version {index + 1}
-        </span>
-        <VersionLabels version={version} colors={AvailableLabelColorsForItem(prompt)} hideChainReferences />
-      </div>
-      <ContentBody isSelected={isSelected}>
-        <TaggedVersionPrompt version={version} />
-      </ContentBody>
-    </div>
-  ) : null
-}
-
-function InputNodeBody({
-  nodes,
-  isSelected,
-  promptCache,
-}: {
-  nodes: ChainNode[]
-  isSelected: boolean
-  promptCache: PromptCache
-}) {
-  const items = nodes.filter(IsChainItem)
-  const variables = ExtractUnboundChainVariables(items, promptCache)
-
-  return variables.length > 0 ? (
-    <ContentBody isSelected={isSelected}>
-      <div className='flex flex-wrap gap-1'>
-        {variables.map((variable, index) => (
-          <span key={index} className={`${InputVariableClass}`}>{`{{${variable}}}`}</span>
-        ))}
-      </div>
-    </ContentBody>
-  ) : null
-}
-
-function ContentBody({ isSelected, children }: { isSelected: boolean; children: ReactNode }) {
-  const colorClass = isSelected ? 'border-blue-100' : 'border-gray-200 bg-white rounded-b-lg'
-  return <div className={`p-3 border-t ${colorClass} max-h-[150px] overflow-y-auto`}>{children}</div>
 }
