82ca5674338f9279b1c6ed689a71b258804fcdd5
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-09-20 10:28:59 +0100

    Fix edge cases with initial state in active item
    E.g. if we push a new URL on deletion, the initial active item and version may change.

--- src/client/hooks/useActiveItem.tsx
+++ src/client/hooks/useActiveItem.tsx
@@ -1,18 +1,24 @@
 import api from '@/src/client/api'
-import { ActiveChain, ActiveProject, ActivePrompt, ChainVersion, IsPromptVersion, PromptVersion } from '@/types'
-import { useState } from 'react'
+import { ActiveChain, ActiveProject, ActivePrompt, IsPromptVersion } from '@/types'
 import { ActiveItem, CompareItem, EndpointsItem } from '@/src/common/activeItem'
+import useInitialState from './useInitialState'
 
 const ActiveItemIsChain = (item: ActiveItem): item is ActiveChain =>
   item !== CompareItem && item !== EndpointsItem && 'referencedItemIDs' in item
 const ActiveItemIsPrompt = (item: ActiveItem): item is ActivePrompt =>
   item !== CompareItem && item !== EndpointsItem && !ActiveItemIsChain(item)
 
+const sameIDs = (a: { id: number } | undefined, b: { id: number } | undefined) => a?.id === b?.id
+const sameItems = (a: ActiveItem | undefined, b: ActiveItem | undefined) =>
+  (a === CompareItem && b === CompareItem) ||
+  (a === EndpointsItem && b === EndpointsItem) ||
+  (a !== CompareItem && a !== EndpointsItem && b !== CompareItem && b !== EndpointsItem && sameIDs(a, b))
+
 export default function useActiveItem(initialActiveProject: ActiveProject, initialActiveItem: ActiveItem | null) {
-  const [activeProject, setActiveProject] = useState(initialActiveProject)
+  const [activeProject, setActiveProject] = useInitialState(initialActiveProject, sameIDs)
   const refreshProject = () => api.getProject(activeProject.id).then(setActiveProject)
 
-  const [activeItem, setActiveItem] = useState(initialActiveItem ?? undefined)
+  const [activeItem, setActiveItem] = useInitialState(initialActiveItem ?? undefined, sameItems)
   const activePrompt = activeItem && ActiveItemIsPrompt(activeItem) ? activeItem : undefined
   const activeChain = activeItem && ActiveItemIsChain(activeItem) ? activeItem : undefined
 
@@ -20,9 +26,9 @@ export default function useActiveItem(initialActiveProject: ActiveProject, initi
 }
 
 export function useActiveVersion(activeItem: ActiveItem | undefined) {
-  const [activeVersion, setActiveVersion] = useState<PromptVersion | ChainVersion | undefined>(
+  const initialActiveVersion =
     activeItem === CompareItem || activeItem === EndpointsItem ? undefined : activeItem?.versions?.slice(-1)?.[0]
-  )
+  const [activeVersion, setActiveVersion] = useInitialState(initialActiveVersion, sameIDs)
   const activePromptVersion = activeVersion && IsPromptVersion(activeVersion) ? activeVersion : undefined
   const activeChainVersion = activeVersion && !IsPromptVersion(activeVersion) ? activeVersion : undefined
 
