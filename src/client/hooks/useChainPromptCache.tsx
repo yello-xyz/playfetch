0b7ca2b5c0cdb38938426e6ff38844b60d4293b3
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-09-22 11:43:50 +0100

    Remove unused chain prompt cache methods

--- src/client/hooks/useChainPromptCache.tsx
+++ src/client/hooks/useChainPromptCache.tsx
@@ -4,10 +4,8 @@ import { ChainNode, IsPromptChainItem } from '../../../components/chains/chainNo
 import useActiveItemCache from './useActiveItemCache'
 
 export type ChainPromptCache = {
-  promptForID: (id: number) => ActivePrompt | undefined
   promptForItem: (item: PromptChainItem) => ActivePrompt | undefined
   versionForItem: (item: PromptChainItem) => PromptVersion | undefined
-  promptItemForID: (promptID: number) => ChainItem
   refreshPrompt: (promptID: number) => void
 }
 
@@ -38,20 +36,9 @@ export default function useChainPromptCache(
   const promptForID = (id: number) => promptCache.itemForID(id) as ActivePrompt | undefined
 
   const chainPromptCache: ChainPromptCache = {
-    promptForID,
     refreshPrompt: promptCache.refreshItem,
     promptForItem: item => promptForID(item.promptID),
     versionForItem: item => promptForID(item.promptID)?.versions.find(version => version.id === item.versionID),
-    promptItemForID: (promptID: number) => {
-      const prompt = project.prompts.find(prompt => prompt.id === promptID)!
-      const versionID = prompt.lastVersionID
-      const cachedPrompt = promptForID(promptID)
-      return {
-        promptID,
-        versionID,
-        ...(cachedPrompt ? { prompt: cachedPrompt, version: cachedPrompt.versions.slice(-1)[0] } : {}),
-      }
-    },
   }
 
   return chainPromptCache
