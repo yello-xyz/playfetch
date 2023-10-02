2b3a1833213ab181ac67b46aa9017f54fac94292
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-02 15:26:50 +0100

    Reset cost and custom models with new API key
    We can't really assume that the custom models will persist across API keys and similarly global provider cost tracking might not make sense when API keys change.

--- src/server/datastore/providers.ts
+++ src/server/datastore/providers.ts
@@ -82,16 +82,7 @@ export async function incrementProviderCostForUser(userID: number, provider: Mod
 export async function saveProviderKey(userID: number, provider: ModelProvider, apiKey: string | null) {
   const providerData = await getProviderData(userID, provider)
   const providerID = providerData ? getID(providerData) : undefined
-  await getDatastore().save(
-    toProviderData(
-      userID,
-      provider,
-      apiKey,
-      providerData?.cost ?? 0,
-      providerData?.customModels ? JSON.parse(providerData.customModels) : [],
-      providerID
-    )
-  )
+  await getDatastore().save(toProviderData(userID, provider, apiKey, 0, [], providerID))
 }
 
 export async function getAvailableProvidersForUser(userID: number): Promise<AvailableProvider[]> {
