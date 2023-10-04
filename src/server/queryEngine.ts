bffb676f5fe9a964722f6b7f869c995e78e4c672
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-04 11:19:28 +0100

    Use user specified credentials for vector store

--- src/server/queryEngine.ts
+++ src/server/queryEngine.ts
@@ -1,4 +1,4 @@
-import { incrementProviderCostForUser } from './datastore/providers'
+import { getProviderCredentials, incrementProviderCostForUser } from './datastore/providers'
 import { APIKeyForProvider, CreateEmbedding } from './providers/integration'
 import runVectorQuery from './providers/pinecone'
 
@@ -9,22 +9,22 @@ type QueryResponse = (
 
 export const runQuery = async (userID: number, indexName: string, query: string): Promise<QueryResponse> => {
   try {
-    const provider = 'openai'
-    const apiKey = await APIKeyForProvider(userID, provider)
-    if (!apiKey) {
+    const embeddingProvider = 'openai'
+    const embeddingApiKey = await APIKeyForProvider(userID, embeddingProvider)
+    if (!embeddingApiKey) {
       throw new Error('Missing API key')
     }
-  
-    const { embedding, cost } = await CreateEmbedding(provider, apiKey, userID, query)
 
-    incrementProviderCostForUser(userID, provider, cost)
+    const queryProvider = 'pinecone'
+    const [queryAPIKey, queryEnvironment] = await getProviderCredentials(userID, queryProvider)
+    if (!queryAPIKey || !queryEnvironment) {
+      throw new Error('Missing vector store credentials')
+    }
 
-    const result = await runVectorQuery(
-      process.env.PINECONE_KEY ?? '',
-      process.env.PINECONE_ENV ?? '',
-      indexName,
-      embedding
-    )
+    const { embedding, cost } = await CreateEmbedding(embeddingProvider, embeddingApiKey, userID, query)
+    incrementProviderCostForUser(userID, embeddingProvider, cost)
+
+    const result = await runVectorQuery(queryAPIKey, queryEnvironment, indexName, embedding)
 
     return { result, output: result.join('\n'), error: undefined, failed: false, cost, attempts: 1 }
   } catch (error: any) {
