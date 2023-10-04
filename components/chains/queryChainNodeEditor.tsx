7bd59c7d7f04a0ea5bb50b3b521e7facc4962748
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-04 14:04:53 +0100

    Add configuration options in query node editor

--- components/chains/queryChainNodeEditor.tsx
+++ components/chains/queryChainNodeEditor.tsx
@@ -1,7 +1,16 @@
 import PromptInput from '../prompts/promptInput'
 import { SingleTabHeader } from '../tabSelector'
 import queryIcon from '@/public/query.svg'
-import { QueryChainItem } from '@/types'
+import { EmbeddingModel, QueryChainItem, QueryProvider } from '@/types'
+import TextInput from '../textInput'
+import Label from '../label'
+import DropdownMenu from '../dropdownMenu'
+import {
+  AllEmbeddingModels,
+  AllQueryProviders,
+  LabelForProvider,
+  ProviderForModel,
+} from '@/src/common/providerMetadata'
 
 export default function QueryChainNodeEditor({
   item,
@@ -10,13 +19,41 @@ export default function QueryChainNodeEditor({
   item: QueryChainItem
   updateItem: (item: QueryChainItem) => void
 }) {
+  const updateProvider = (provider: QueryProvider) => updateItem({ ...item, provider })
+  const updateModel = (model: EmbeddingModel) =>
+    updateItem({ ...item, embeddingProvider: ProviderForModel(model), embeddingModel: model })
+  const updateIndexName = (indexName: string) => updateItem({ ...item, indexName })
   const updateQuery = (query: string) => updateItem({ ...item, query })
+
+  const gridConfig = 'grid grid-cols-[160px_minmax(0,1fr)]'
 
   return (
     <>
       <div className='flex flex-col w-full overflow-y-auto'>
         <SingleTabHeader label='Query' icon={queryIcon} />
-        <div className='flex flex-col gap-2 px-4 pt-4 min-h-[160px]'>
+        <div className='flex flex-col gap-2 px-4 pt-4 min-h-[350px]'>
+          <span className='font-medium'>Configuration</span>
+          <div
+            className={`${gridConfig} w-full items-center gap-4 p-4 py-4 bg-white border-gray-200 border rounded-lg`}>
+            <Label>Vector Store</Label>
+            <DropdownMenu value={item.provider} onChange={value => updateProvider(value as QueryProvider)}>
+              {AllQueryProviders.map(provider => (
+                <option key={provider} value={provider}>
+                  {LabelForProvider(provider)}
+                </option>
+              ))}
+            </DropdownMenu>
+            <Label>Embedding Model</Label>
+            <DropdownMenu value={item.embeddingModel} onChange={value => updateModel(value as EmbeddingModel)}>
+              {AllEmbeddingModels.map(model => (
+                <option key={model} value={model}>
+                  {`${LabelForProvider(ProviderForModel(model))} - ${model}`}
+                </option>
+              ))}
+            </DropdownMenu>
+            <Label>Index name</Label>
+            <TextInput value={item.indexName} setValue={updateIndexName} />
+          </div>
           <span className='font-medium mt-2.5'>Query</span>
           <PromptInput placeholder='Query' value={item.query} setValue={updateQuery} preformatted />
         </div>
