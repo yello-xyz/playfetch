afd3a6e3212d0d9605b2823b66f8fe683ab3fb5c
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-12 10:38:33 +0100

    Factor out consumeStream helper

--- src/client/hooks/useRunVersion.tsx
+++ src/client/hooks/useRunVersion.tsx
@@ -1,7 +1,44 @@
-import api from '@/src/client/api'
+import api, { StreamReader } from '@/src/client/api'
 import { useState } from 'react'
 import { PartialRun, PromptInputs } from '@/types'
 import { useRefreshActiveItem } from '../context/refreshContext'
+
+export const consumeStream = async (
+  inputs: PromptInputs[],
+  streamReader: StreamReader,
+  callback: (runs: PartialRun[]) => void
+) => {
+  const runs = Object.fromEntries(inputs.map((_, inputIndex) => [inputIndex, {} as { [index: number]: PartialRun }]))
+  while (streamReader) {
+    const { done, value } = await streamReader.read()
+    if (done) {
+      break
+    }
+    const text = await new Response(value).text()
+    const lines = text.split('\n')
+    for (const line of lines.filter(line => line.trim().length > 0)) {
+      const data = line.split('data:').slice(-1)[0]
+      const { inputIndex, configIndex, index, message, cost, duration, timestamp, failed, isLast } = JSON.parse(data)
+      if (isLast) {
+        runs[inputIndex][index].isLast = true
+      } else {
+        const previousOutput = runs[inputIndex][index]?.output ?? ''
+        const output = message ? `${previousOutput}${message}` : previousOutput
+        runs[inputIndex][index] = { id: index, index: configIndex, output, cost, duration, timestamp, failed }
+      }
+    }
+    const maxSteps = Math.max(...Object.values(runs).map(runs => Object.keys(runs).length))
+    const sortedRuns = Object.entries(runs)
+      .flatMap(([inputIndex, inputRuns]) =>
+        Object.entries(inputRuns).map(([index, run]) => ({
+          ...run,
+          id: Number(inputIndex) * maxSteps + Number(index),
+        }))
+      )
+      .sort((a, b) => a.id - b.id)
+    callback(sortedRuns)
+  }
+}
 
 export default function useRunVersion(clearLastPartialRunsOnCompletion = false) {
   const refreshActiveItem = useRefreshActiveItem()
@@ -15,37 +52,10 @@ export default function useRunVersion(clearLastPartialRunsOnCompletion = false) 
     setHighestRunIndex(-1)
     const versionID = await getVersion()
     const streamReader = await api.runVersion(versionID, inputs)
-    const runs = Object.fromEntries(inputs.map((_, inputIndex) => [inputIndex, {} as { [index: number]: PartialRun }]))
-    while (streamReader) {
-      const { done, value } = await streamReader.read()
-      if (done) {
-        break
-      }
-      const text = await new Response(value).text()
-      const lines = text.split('\n')
-      for (const line of lines.filter(line => line.trim().length > 0)) {
-        const data = line.split('data:').slice(-1)[0]
-        const { inputIndex, configIndex, index, message, cost, duration, timestamp, failed, isLast } = JSON.parse(data)
-        if (isLast) {
-          runs[inputIndex][index].isLast = true
-        } else {
-          const previousOutput = runs[inputIndex][index]?.output ?? ''
-          const output = message ? `${previousOutput}${message}` : previousOutput
-          runs[inputIndex][index] = { id: index, index: configIndex, output, cost, duration, timestamp, failed }
-        }
-      }
-      const maxSteps = Math.max(...Object.values(runs).map(runs => Object.keys(runs).length))
-      const sortedRuns = Object.entries(runs)
-        .flatMap(([inputIndex, inputRuns]) =>
-          Object.entries(inputRuns).map(([index, run]) => ({
-            ...run,
-            id: Number(inputIndex) * maxSteps + Number(index),
-          }))
-        )
-        .sort((a, b) => a.id - b.id)
-      setPartialRuns(sortedRuns)
-      setHighestRunIndex(Math.max(highestRunIndex, ...sortedRuns.map(run => run.index ?? 0)))
-    }
+    await consumeStream(inputs, streamReader, runs => {
+      setPartialRuns(runs)
+      setHighestRunIndex(Math.max(highestRunIndex, ...runs.map(run => run.index ?? 0)))
+    })
     await refreshActiveItem(versionID)
 
     if (clearLastPartialRunsOnCompletion) {
