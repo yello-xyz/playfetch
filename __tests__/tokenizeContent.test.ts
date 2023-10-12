4f9cf02844ab8b436fab316b97728e1d6b5ef2f6
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-12 13:33:37 +0100

    Add some tests for tokenization
    Including truncation, detecting or stripping sentinels and diffing content.

---
+++ __tests__/tokenizeContent.test.ts
@@ -0,0 +1,49 @@
+import { TokenizeContent } from '@/components/versions/versionComparison'
+
+type Token = { state: '=' | '-' | '+'; content: string; tagged: boolean }
+
+const testTokenizeContent = (
+  testDescription: string,
+  content: string,
+  expectedTokens: Token[],
+  compareContent?: string,
+  maxLength = 0,
+  stripSentinels = false
+) =>
+  test(`Test ${testDescription}`, () => {
+    expect(TokenizeContent(content, compareContent, maxLength, stripSentinels)).toStrictEqual(expectedTokens)
+  })
+
+const token = (content: string, state: Token['state'] = '=', tagged = false): Token => ({ state, content, tagged })
+
+testTokenizeContent('empty content', '', [])
+testTokenizeContent('simple content', 'hello world', [token('hello world')])
+
+testTokenizeContent('truncate', 'hello world how are you', [token('hello … ')], undefined, 1)
+testTokenizeContent('truncate limit', 'hello world', [token('hello world')], undefined, 1)
+
+testTokenizeContent('tagged content', 'hello {{world}}', [token('hello '), token('{{world}}', '=', true)])
+testTokenizeContent(
+  'strip sentinels',
+  'hello {{world}}',
+  [token('hello '), token('world', '=', true)],
+  undefined,
+  0,
+  true
+)
+
+testTokenizeContent('added content', 'hello world', [token('hello'), token(' world', '+')], 'hello')
+testTokenizeContent('removed content', 'hello', [token('hello'), token(' world', '-')], 'hello world')
+testTokenizeContent(
+  'changed content',
+  'goodbye world',
+  [token('hello', '-'), token('goodbye', '+'), token(' world')],
+  'hello world'
+)
+testTokenizeContent(
+  'added stripped sentinels',
+  'hello {{world}}',
+  [token('hello '), token('world', '-'), token('', '+'), token('world', '+', true)],
+  'hello world',
+  0, true
+)
