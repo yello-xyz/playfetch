9671bf9a0c3f7f0aaedfc82c8dbcaf24a7215641
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-16 10:08:44 +0100

    Move admin links to sidebar

--- components/admin/adminSidebar.tsx
+++ components/admin/adminSidebar.tsx
@@ -1,13 +1,18 @@
-import fileIcon from '@/public/file.svg'
-import UserSidebarItem from '../userSidebarItem'
-import { useLoggedInUser } from '@/src/client/context/userContext'
+import { AdminRoute } from '@/src/client/clientRoute'
 import { FeedbackSection, SidebarButton, SidebarSection } from '../sidebar'
+import linkIcon from '@/public/chainSmall.svg'
 
 export default function AdminSidebar({}: {}) {
   return (
     <>
       <div className='flex flex-col gap-4 px-2 pt-3 pb-4 border-r border-gray-200 bg-gray-25'>
-        <SidebarSection className='flex-1'>
+        <SidebarSection title='Analytics'>
+          <SidebarButton title='Analytics Dashboard' icon={linkIcon} link={AdminRoute.AnalyticsDashboard} />
+          <SidebarButton title='Analytics Reports' icon={linkIcon} link={AdminRoute.AnalyticsReports} />
+          <SidebarButton title='Search Console' icon={linkIcon} link={AdminRoute.SearchConsole} />
+        </SidebarSection>
+        <SidebarSection className='flex-1' title='Debug'>
+          <SidebarButton title='Server Logs' icon={linkIcon} link={AdminRoute.ServerLogs} />
         </SidebarSection>
         <FeedbackSection />
       </div>
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,7 @@
 import { withAdminSession } from '@/src/server/session'
-import { ReactNode } from 'react'
 import { User } from '@/types'
 import { getUsersWithoutAccess } from '@/src/server/datastore/users'
 import TopBar, { TopBarAccessoryItem, TopBarBackItem } from '@/components/topBar'
-import { AdminRoute } from '@/src/client/clientRoute'
-import Link from 'next/link'
-import Icon from '@/components/icon'
-import chainIcon from '@/public/chainSmall.svg'
 import AdminSidebar from '@/components/admin/adminSidebar'
 import Waitlist from '@/components/admin/waitlist'
 
@@ -28,12 +23,6 @@ export default function Admin({ initialWaitlistUsers }: { initialWaitlistUsers: 
         <div className='flex items-stretch flex-1 overflow-hidden'>
           <AdminSidebar />
           <div className='flex flex-col flex-1 bg-gray-25'>
-            <div className='flex flex-col gap-4 p-4'>
-            <ExternalLink href={AdminRoute.AnalyticsDashboard}>Analytics Dashboard</ExternalLink>
-            <ExternalLink href={AdminRoute.AnalyticsReports}>Analytics Reports</ExternalLink>
-            <ExternalLink href={AdminRoute.SearchConsole}>Search Console</ExternalLink>
-            <ExternalLink href={AdminRoute.ServerLogs}>Server Logs</ExternalLink>
-            </div>
             <Waitlist initialWaitlistUsers={initialWaitlistUsers} />
           </div>
         </div>
@@ -41,10 +30,3 @@ export default function Admin({ initialWaitlistUsers }: { initialWaitlistUsers: 
     </>
   )
 }
-
-const ExternalLink = ({ href, children }: { href: string; children: ReactNode }) => (
-  <Link className='flex items-center gap-1 underline' href={href} target='_blank'>
-    <Icon className='-rotate-45' icon={chainIcon} />
-    {children}
-  </Link>
-)
