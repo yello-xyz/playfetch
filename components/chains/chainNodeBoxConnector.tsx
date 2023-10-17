5a60ec57f2e1ababcb799afa9b7de6c992c2dbd0
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-17 16:40:36 +0100

    Allow to dismiss insert chain step button
    Close #72

--- components/chains/chainNodeBoxConnector.tsx
+++ components/chains/chainNodeBoxConnector.tsx
@@ -3,6 +3,7 @@ import { Prompt } from '@/types'
 import promptIcon from '@/public/prompt.svg'
 import codeIcon from '@/public/code.svg'
 import queryIcon from '@/public/query.svg'
+import closeIcon from '@/public/closeWhite.svg'
 import Icon from '../icon'
 import addIcon from '@/public/addSmall.svg'
 import addActiveIcon from '@/public/addWhiteSmall.svg'
@@ -117,7 +118,7 @@ const AddButton = ({
     <>
       <DownArrow height='min-h-[38px]' />
       <SmallDot margin='-mb-[5px] mt-1' color='bg-blue-200' />
-      <div ref={buttonRef} className='flex border border-blue-100 border-dashed rounded-lg w-96 bg-blue-25'>
+      <div ref={buttonRef} className='relative flex border border-blue-100 border-dashed rounded-lg w-96 bg-blue-25'>
         <AddStepButton label='Add prompt' className={buttonClass} icon={promptIcon} onClick={togglePopup} />
         {onInsertQuery && (
           <>
@@ -127,6 +128,11 @@ const AddButton = ({
         )}
         <DownStroke color='border-blue-100' />
         <AddStepButton label='Add code block' className={buttonClass} icon={codeIcon} onClick={insertCode} />
+        <div
+          className='absolute flex items-center justify-center w-5 h-5 bg-blue-200 rounded-full -top-2.5 -right-2.5 hover:bg-blue-400 hover:cursor-pointer'
+          onClick={toggleActive()}>
+          <Icon icon={closeIcon} />
+        </div>
       </div>
       <SmallDot margin='-mt-[5px] mb-0.5' color='bg-blue-200' />
       <DownStroke height='min-h-[32px]' />
