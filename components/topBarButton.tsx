9a1d332f896403edf6ac343b7038053301959d5d
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-17 09:23:46 +0100

    Show name of project users when hovering avatars

--- components/topBarButton.tsx
+++ components/topBarButton.tsx
@@ -48,8 +48,13 @@ export function UserAvatars({ users }: { users: User[] }) {
       className='flex flex-row-reverse space-x-reverse -space-x-[50px]'
       style={{ marginRight: `${(users.length - 1) * 22}px` }}>
       {users.map((user, index) => (
-        <div key={index}>
+        <div key={index} className='relative group'>
           <UserAvatar user={user} border />
+          <div className='absolute left-3.5 z-10 justify-center hidden overflow-visible group-hover:flex top-8 max-w-0'>
+            <span className='px-1 text-xs font-medium text-gray-600 bg-white border border-gray-200 rounded whitespace-nowrap'>
+              {user.fullName}
+            </span>
+          </div>
         </div>
       ))}
     </div>
