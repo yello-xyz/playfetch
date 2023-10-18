9f10ecde71a6fdaf7303f7acac9c2603b8c03d25
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-18 15:23:46 +0100

    Factor out InviteCell component
    Should be able to reuse this for projects later.

--- components/workspaces/workspaceInvite.tsx
+++ components/workspaces/workspaceInvite.tsx
@@ -1,4 +1,4 @@
-import { PendingWorkspace } from '@/types'
+import { PendingWorkspace, User } from '@/types'
 import WorkspaceTopBar from './workspaceTopBar'
 import { FormatRelativeDate } from '@/src/common/formatting'
 import Button from '../button'
@@ -9,24 +9,40 @@ export default function WorkspaceInvite({ workspace }: { workspace: PendingWorks
     <div className='flex flex-col h-full'>
       <WorkspaceTopBar activeWorkspace={workspace} />
       <div className='p-6'>
-        <div className='flex items-center justify-between w-full gap-6 px-4 py-3 border border-gray-200 rounded-lg select-none bg-gray-25'>
-          <div className='flex flex-col gap-2'>
-            <span className='flex-1 text-base font-medium text-gray-700 line-clamp-2'>{workspace.name}</span>
-            <div className='flex items-center gap-2 text-sm'>
-              <UserAvatar size='md' user={workspace.invitedBy} />
-              <span className='text-gray-700'>{workspace.invitedBy.fullName} invited you</span>
-              <span className='-ml-1 text-gray-400'>• {FormatRelativeDate(workspace.timestamp)}</span>
-            </div>
-          </div>
-          <div className='flex items-center gap-2'>
-            <Button type='outline' onClick={() => {}}>
-              Decline
-            </Button>
-            <Button type='primary' onClick={() => {}}>
-              Accept
-            </Button>
-          </div>
+        <InviteCell item={workspace} onRespond={() => {}} />
+      </div>
+    </div>
+  )
+}
+
+function InviteCell({
+  item,
+  onRespond,
+}: {
+  item: {
+    name: string
+    invitedBy: User
+    timestamp: number
+  }
+  onRespond: (accept: boolean) => void
+}) {
+  return (
+    <div className='flex items-center justify-between w-full gap-6 px-4 py-3 border border-gray-200 rounded-lg select-none bg-gray-25'>
+      <div className='flex flex-col gap-2'>
+        <span className='flex-1 text-base font-medium text-gray-700 line-clamp-2'>{item.name}</span>
+        <div className='flex items-center gap-2 text-sm'>
+          <UserAvatar size='md' user={item.invitedBy} />
+          <span className='text-gray-700'>{item.invitedBy.fullName} invited you</span>
+          <span className='-ml-1 text-gray-400'>• {FormatRelativeDate(item.timestamp)}</span>
         </div>
+      </div>
+      <div className='flex items-center gap-2'>
+        <Button type='outline' onClick={() => onRespond(false)}>
+          Decline
+        </Button>
+        <Button type='primary' onClick={() => onRespond(true)}>
+          Accept
+        </Button>
       </div>
     </div>
   )
