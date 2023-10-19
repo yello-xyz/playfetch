33a3d90f0dc76fbb62381d11340482b82df16ee1
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-19 10:19:20 +0100

    Show invitation info with pending invites

--- components/users/inviteButton.tsx
+++ components/users/inviteButton.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react'
-import { CheckValidEmail } from '@/src/common/formatting'
+import { CheckValidEmail, FormatRelativeDate } from '@/src/common/formatting'
 import useGlobalPopup, { GlobalPopupLocation, WithDismiss } from '@/src/client/context/globalPopupContext'
 import { PopupContent } from '../popupMenu'
 import { CustomPopupButton } from '../popupButton'
@@ -8,6 +8,7 @@ import { PendingUser, User } from '@/types'
 import TextInput from '../textInput'
 import { TopBarButton } from '../topBarButton'
 import UserBadge from './userBadge'
+import useFormattedDate from '@/src/client/hooks/useFormattedDate'
 
 export default function InviteButton({
   users,
@@ -67,7 +68,7 @@ function InvitePopup({ users, pendingUsers, onInvite, withDismiss }: InvitePopup
         <>
           <SectionHeader>Pending invitations</SectionHeader>
           {pendingUsers.map((user, index) => (
-            <UserBadge key={index} user={user} padding='' />
+            <PendingUserBadge key={index} user={user} />
           ))}
         </>
       )}
@@ -78,3 +79,21 @@ function InvitePopup({ users, pendingUsers, onInvite, withDismiss }: InvitePopup
 const SectionHeader = ({ children }: { children: string }) => (
   <span className='text-xs font-medium text-gray-400'>{children}</span>
 )
+
+function PendingUserBadge({ user }: { user: PendingUser }) {
+  const formattedDate = useFormattedDate(user.timestamp, FormatRelativeDate)
+
+  return (
+    <div className='flex items-center justify-between gap-2'>
+      <div className='flex-1 min-w-0'>
+        <UserBadge user={user} padding='' />
+      </div>
+      <div className='flex flex-col items-end text-xs'>
+        <span className='text-gray-700'>
+          invited by <span className='font-medium'>{user.invitedBy.fullName}</span>
+        </span>
+        <span className='text-gray-400'>{formattedDate}</span>
+      </div>
+    </div>
+  )
+}
