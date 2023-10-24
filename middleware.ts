759da5ff72bee4db91dd010146f26f0053540344
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-10-24 14:03:27 +0100

    Don't require https for cron requests
    Looks like it's failing with 301 because of this. Shouldn't matter as these calls will be made within Google Cloud infra.

--- middleware.ts
+++ middleware.ts
@@ -10,12 +10,13 @@ const GetSecureHeader = (getHeader: GetHeader) => getHeader('x-forwarded-proto')
 const IsLocalHost = (getHeader: GetHeader) => GetHost(getHeader).includes('localhost')
 const IsSecure = (getHeader: GetHeader) => GetSecureHeader(getHeader) === HTTTPS
 const GetSecureURL = (getHeader: GetHeader) => new URL(`${HTTTPS}://${GetHost(getHeader)}`)
+const IsCronRequest = (getHeader: GetHeader) => getHeader('x-appengine-cron') === 'true'
 
 export function middleware(request: NextRequest) {
   const headers = new Headers(request.headers)
   const getHeader = (key: string) => headers.get(key)
 
-  if (!IsSecure(getHeader) && !IsLocalHost(getHeader)) {
+  if (!IsSecure(getHeader) && !IsLocalHost(getHeader) && !IsCronRequest(getHeader)) {
     return NextResponse.redirect(GetSecureURL(getHeader).href, 301)
   } else {
     return NextResponse.next()
