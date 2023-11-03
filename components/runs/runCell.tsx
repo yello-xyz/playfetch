142d73fdb8435b65111e1a5f87536982be41008b
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-11-03 12:40:31 +0000

    Move commenting logic from RunCell to RunCellBody
    Should make it easier to reuse this in continuation fragments too.

--- components/runs/runCell.tsx
+++ components/runs/runCell.tsx
@@ -1,12 +1,7 @@
 import { ActiveChain, ActivePrompt, ChainVersion, PartialRun, PromptVersion, Run } from '@/types'
-import { MouseEvent, useCallback } from 'react'
-import { CommentsPopup, CommentsPopupProps } from '../commentPopupMenu'
-import { AvailableLabelColorsForItem } from '../labelPopupMenu'
 import RunCellHeader from './runCellHeader'
 import RunCellFooter from './runCellFooter'
 import RunCellBody from './runCellBody'
-import useGlobalPopup from '@/src/client/context/globalPopupContext'
-import CommentInputPopup, { CommentInputProps, CommentSelection, useExtractCommentSelection } from './commentInputPopup'
 import RunCellContinuation from './runCellContinuation'
 
 export default function RunCell({
@@ -24,85 +19,7 @@ export default function RunCell({
   isRunning?: boolean
   runContinuation?: (continuationID: number, message: string) => void
 }) {
-  const comments = (version?.comments ?? []).filter(comment => comment.runID === run.id)
-
-  const setPopup = useGlobalPopup<CommentInputProps | CommentsPopupProps>()
-
-  const selectComment = (event: MouseEvent, startIndex: number) => {
-    if (version && activeItem) {
-      const popupComments = comments.filter(comment => comment.startIndex === startIndex)
-      setPopup(
-        props => CommentsPopup(props as CommentsPopupProps),
-        {
-          comments: popupComments,
-          versionID: version.id,
-          selection: popupComments[0].quote,
-          runID: run.id,
-          startIndex,
-          users: activeItem.users,
-          labelColors: AvailableLabelColorsForItem(activeItem),
-        },
-        { left: event.clientX - 200, top: event.clientY + 20 }
-      )
-    }
-  }
-
-  const selectionRanges = comments
-    .filter(comment => comment.startIndex !== undefined && comment.quote)
-    .map(comment => ({
-      startIndex: comment.startIndex!,
-      endIndex: comment.startIndex! + comment.quote!.length,
-    }))
-
-  const updateSelectionForComment = useCallback(
-    (selection?: CommentSelection) => {
-      if (selection && version && activeItem) {
-        let selectionForComment = selection
-        const start = selection.startIndex
-        const end = start + selection.text.length
-        const existingRange = selectionRanges.find(
-          ({ startIndex, endIndex }) =>
-            (start >= startIndex && start < endIndex) || (end > startIndex && end <= endIndex)
-        )
-        if (existingRange) {
-          const text = run.output.substring(existingRange.startIndex, existingRange.endIndex)
-          selectionForComment = { ...selection, startIndex: existingRange.startIndex, text }
-        }
-        const selectionComments = comments.filter(comment => comment.startIndex === selectionForComment.startIndex)
-        setPopup(
-          props => CommentsPopup(props as CommentsPopupProps),
-          {
-            comments: selectionComments,
-            versionID: version.id,
-            selection: selectionForComment.text,
-            runID: run.id,
-            startIndex: selectionForComment.startIndex,
-            users: activeItem.users,
-            labelColors: AvailableLabelColorsForItem(activeItem),
-          },
-          { left: selectionForComment.popupPoint.x - 160, top: selectionForComment.popupPoint.y }
-        )
-      }
-    },
-    [activeItem, version, comments, run, selectionRanges, setPopup]
-  )
-
-  const onUpdateSelection = useCallback(
-    (selection: CommentSelection | undefined) => {
-      if (selection && version) {
-        setPopup(
-          props => CommentInputPopup(props as CommentInputProps),
-          { selection, onUpdateSelectionForComment: updateSelectionForComment },
-          { left: selection.popupPoint.x, top: selection.popupPoint.y }
-        )
-      }
-    },
-    [version, setPopup, updateSelectionForComment]
-  )
-
   const isProperRun = ((item): item is Run => 'labels' in item)(run)
-  useExtractCommentSelection(isProperRun ? identifier : null, onUpdateSelection)
-
   const continuationID = run.continuationID
   const isContinuation = !!continuationID
 
@@ -117,9 +34,9 @@ export default function RunCell({
         <div className='flex flex-col gap-2.5 flex-1'>
           <RunCellBody
             identifier={identifier}
-            output={run.output}
-            selectionRanges={selectionRanges}
-            onSelectComment={selectComment}
+            run={run}
+            version={version}
+            activeItem={activeItem}
             isContinuation={isContinuation}
           />
         </div>
