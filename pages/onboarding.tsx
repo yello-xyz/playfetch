fb4b403f2c91cd7a61ffa466734654f699f58778
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-11-06 15:49:38 +0000

    Factor out OnboardingStep component

--- pages/onboarding.tsx
+++ pages/onboarding.tsx
@@ -1,7 +1,7 @@
 import { withLoggedInSession } from '@/src/server/session'
 import ClientRoute, { Redirect } from '@/src/common/clientRoute'
 import Button from '@/components/button'
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
 
 export const getServerSideProps = withLoggedInSession(async ({ user }) =>
   user.didCompleteOnboarding ? Redirect(ClientRoute.Home) : { props: {} }
@@ -52,33 +52,60 @@ export default function Onboarding() {
 
   return (
     <main className='flex items-center justify-center h-screen p-10 text-gray-700 bg-gray-25'>
-      <div className='flex flex-col w-full gap-1 p-6 bg-white shadow rounded-lg border border-gray-200 max-w-[536px]'>
-        <span className='text-2xl font-bold tracking-tight'>What are you looking to use PlayFetch for?</span>
-        <span className='text-xl pb-7'>Select all that apply.</span>
-        {step === 'useCase' && <UseCase response={response} setResponse={setResponse} onNext={() => setStep('role')} />}
-        {step === 'role' && <Role response={response} setResponse={setResponse} onNext={() => setStep('area')} />}
-        {step === 'area' && <Area response={response} setResponse={setResponse} onNext={() => {}} />}
-      </div>
+      {step === 'useCase' && (
+        <UseCaseStep response={response} setResponse={setResponse} onNextStep={() => setStep('role')} />
+      )}
+      {step === 'role' && <RoleStep response={response} setResponse={setResponse} onNextStep={() => setStep('area')} />}
+      {step === 'area' && <AreaStep response={response} setResponse={setResponse} onNextStep={() => {}} />}
     </main>
   )
 }
 
-const UseCase = ({
+const OnboardingStep = ({
+  title,
+  subtitle = 'Help us personalise your experience',
+  isValid,
+  onNextStep,
+  children,
+}: {
+  title: string
+  subtitle?: string
+  isValid: boolean
+  onNextStep: () => void
+  children: ReactNode
+}) => (
+  <div className='flex flex-col w-full gap-1 p-6 bg-white shadow rounded-lg border border-gray-200 max-w-[536px]'>
+    <span className='text-2xl font-bold tracking-tight'>{title}</span>
+    <span className='text-xl pb-7'>{subtitle}</span>
+    <div className='flex flex-col gap-4 text-base'>{children}</div>
+    <div className='self-end pt-4'>
+      <Button disabled={!isValid} onClick={onNextStep}>
+        Next
+      </Button>
+    </div>
+  </div>
+)
+
+const UseCaseStep = ({
   response,
   setResponse,
-  onNext,
+  onNextStep,
 }: {
   response: OnboardingResponse
   setResponse: (response: OnboardingResponse) => void
-  onNext: () => void
+  onNextStep: () => void
 }) => {
   const [otherUseCase, setOtherUseCase] = useState<string>('')
   const useCase = response.useCase
   const setUseCase = (useCase: OnboardingResponse['useCase']) => setResponse({ ...response, useCase })
-  const isValidUseCase = JSON.stringify(useCase) !== JSON.stringify(emptyUseCase) || otherUseCase.trim() !== ''
+  const isValidUseCase = Object.values(useCase).some(u => u) || otherUseCase.trim() !== ''
 
   return (
-    <div className='flex flex-col gap-4 text-base'>
+    <OnboardingStep
+      title='What are you looking to use PlayFetch for?'
+      subtitle='Select all that apply.'
+      isValid={isValidUseCase}
+      onNextStep={onNextStep}>
       <OptionCheckbox title='Prompt iteration' value={useCase} setValue={setUseCase} valueKey='iteration' />
       <OptionCheckbox title='Prompt testing' value={useCase} setValue={setUseCase} valueKey='testing' />
       <OptionCheckbox
@@ -95,12 +122,56 @@ const UseCase = ({
         setValue={setUseCase}
         valueKey='feedback'
       />
-      <div className='self-end pt-4'>
-        <Button disabled={!isValidUseCase} onClick={onNext}>
-          Next
-        </Button>
-      </div>
-    </div>
+    </OnboardingStep>
+  )
+}
+
+const RoleStep = ({
+  response,
+  setResponse,
+  onNextStep,
+}: {
+  response: OnboardingResponse
+  setResponse: (response: OnboardingResponse) => void
+  onNextStep: () => void
+}) => {
+  const role = response.role
+  const setRole = (role: OnboardingResponse['role']) => setResponse({ ...response, role })
+  const isValidRole = role !== undefined
+
+  return (
+    <OnboardingStep title='What is your role?' isValid={isValidRole} onNextStep={onNextStep}>
+      <RoleOption title='Executive' value={role} setValue={setRole} role='executive' />
+      <RoleOption title='Management' value={role} setValue={setRole} role='manager' />
+      <RoleOption title='Individual Contributor' value={role} setValue={setRole} role='individual' />
+      <RoleOption title='Freelance or Contractor' value={role} setValue={setRole} role='contractor' />
+    </OnboardingStep>
+  )
+}
+
+const AreaStep = ({
+  response,
+  setResponse,
+  onNextStep,
+}: {
+  response: OnboardingResponse
+  setResponse: (response: OnboardingResponse) => void
+  onNextStep: () => void
+}) => {
+  const [otherArea, setOtherArea] = useState<string>('')
+  const area = response.area
+  const setArea = (area: OnboardingResponse['area']) => setResponse({ ...response, area })
+  const isValidArea = Object.values(area).some(a => a) || otherArea.trim() !== ''
+
+  return (
+    <OnboardingStep title='What kind of work do you do?' isValid={isValidArea} onNextStep={onNextStep}>
+      <OptionCheckbox title='Product' value={area} setValue={setArea} valueKey='product' />
+      <OptionCheckbox title='Engineering' value={area} setValue={setArea} valueKey='engineering' />
+      <OptionCheckbox title='Marketing' value={area} setValue={setArea} valueKey='marketing' />
+      <OptionCheckbox title='Content Strategy' value={area} setValue={setArea} valueKey='content' />
+      <OptionCheckbox title='Design' value={area} setValue={setArea} valueKey='design' />
+      <OptionCheckbox title='Sales' value={area} setValue={setArea} valueKey='sales' />
+    </OnboardingStep>
   )
 }
 
@@ -131,34 +202,6 @@ const OptionCheckbox = <T extends Record<string, boolean>>({
   )
 }
 
-const Role = ({
-  response,
-  setResponse,
-  onNext,
-}: {
-  response: OnboardingResponse
-  setResponse: (response: OnboardingResponse) => void
-  onNext: () => void
-}) => {
-  const role = response.role
-  const setRole = (role: OnboardingResponse['role']) => setResponse({ ...response, role })
-  const isValidRole = role !== undefined
-
-  return (
-    <div className='flex flex-col gap-4 text-base'>
-      <RoleOption title='Executive' value={role} setValue={setRole} role='executive' />
-      <RoleOption title='Management' value={role} setValue={setRole} role='manager' />
-      <RoleOption title='Individual Contributor' value={role} setValue={setRole} role='individual' />
-      <RoleOption title='Freelance or Contractor' value={role} setValue={setRole} role='contractor' />
-      <div className='self-end pt-4'>
-        <Button disabled={!isValidRole} onClick={onNext}>
-          Next
-        </Button>
-      </div>
-    </div>
-  )
-}
-
 const RoleOption = ({
   title,
   value,
@@ -182,37 +225,6 @@ const RoleOption = ({
       <label className='font-medium cursor-pointer' htmlFor={value?.toString()}>
         {title}
       </label>
-    </div>
-  )
-}
-
-const Area = ({
-  response,
-  setResponse,
-  onNext,
-}: {
-  response: OnboardingResponse
-  setResponse: (response: OnboardingResponse) => void
-  onNext: () => void
-}) => {
-  const [otherArea, setOtherArea] = useState<string>('')
-  const area = response.area
-  const setArea = (area: OnboardingResponse['area']) => setResponse({ ...response, area })
-  const isValidArea = JSON.stringify(area) !== JSON.stringify(emptyArea) || otherArea.trim() !== ''
-
-  return (
-    <div className='flex flex-col gap-4 text-base'>
-      <OptionCheckbox title='Product' value={area} setValue={setArea} valueKey='product' />
-      <OptionCheckbox title='Engineering' value={area} setValue={setArea} valueKey='engineering' />
-      <OptionCheckbox title='Marketing' value={area} setValue={setArea} valueKey='marketing' />
-      <OptionCheckbox title='Content Strategy' value={area} setValue={setArea} valueKey='content' />
-      <OptionCheckbox title='Design' value={area} setValue={setArea} valueKey='design' />
-      <OptionCheckbox title='Sales' value={area} setValue={setArea} valueKey='sales' />
-      <div className='self-end pt-4'>
-        <Button disabled={!isValidArea} onClick={onNext}>
-          Next
-        </Button>
-      </div>
     </div>
   )
 }
