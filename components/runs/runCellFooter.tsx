85fa572845ecaeb6e5aa074707145c818d3078c0
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-11-08 10:35:54 +0000

    Add icons to run cell footer

--- components/runs/runCellFooter.tsx
+++ components/runs/runCellFooter.tsx
@@ -2,24 +2,29 @@ import { FormatCost, FormatDuration } from '@/src/common/formatting'
 import { PartialRun, Run } from '@/types'
 import useFormattedDate from '@/src/client/hooks/useFormattedDate'
 import { BorderedSection } from './runCellContinuation'
+import durationIcon from '@/public/duration.svg'
+import costIcon from '@/public/cost.svg'
+import dateIcon from '@/public/date.svg'
+import Icon from '../icon'
+import { StaticImageData } from 'next/image'
 
 export default function RunCellFooter({ run, isContinuation }: { run: PartialRun | Run; isContinuation: boolean }) {
   const formattedDate = useFormattedDate(run.timestamp)
 
-  const attributes = [] as string[]
-  if (run.duration) {
-    attributes.push(FormatDuration(run.duration))
-  }
-  if (run.cost) {
-    attributes.push(FormatCost(run.cost))
-  }
-  if (formattedDate) {
-    attributes.push(formattedDate)
-  }
-
-  return attributes.length > 0 ? (
+  return run.duration || run.cost || formattedDate ? (
     <BorderedSection border={isContinuation} bridgingGap>
-      <div className='self-end text-xs text-gray-500'>{attributes.join(' · ')}</div>
+      <div className='flex items-center gap-3'>
+      {run.duration && <Attribute icon={durationIcon} value={FormatDuration(run.duration)} />}
+      {run.cost && <Attribute icon={costIcon} value={FormatCost(run.cost)} />}
+      {formattedDate && <Attribute icon={dateIcon} value={formattedDate} />}
+      </div>
     </BorderedSection>
   ) : null
 }
+
+const Attribute = ({ icon, value }: { icon: StaticImageData; value: string }) => (
+  <div className='flex items-center'>
+    <Icon icon={icon} />
+    <div className='pt-0.5 text-xs text-gray-500'>{value}</div>
+  </div>
+)
