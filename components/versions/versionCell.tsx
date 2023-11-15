067cc1a004dea13b0030ea2cd08279057ec7da1e
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-11-15 16:29:21 +0000

    Factor out VersionCellHeader component

--- components/versions/versionCell.tsx
+++ components/versions/versionCell.tsx
@@ -1,20 +1,7 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode } from 'react'
 import { ActivePrompt, PromptVersion, ChainVersion, IsPromptVersion, ActiveChain } from '@/types'
-import VersionPopupMenu from './versionPopupMenu'
 import VersionComparison from './versionComparison'
-import LabelPopupMenu from '../labelPopupMenu'
-import UserAvatar from '@/components/users/userAvatar'
-import CommentPopupMenu from '../commentPopupMenu'
-import useFormattedDate from '@/src/client/hooks/useFormattedDate'
-import { FormatRelativeDate } from '@/src/common/formatting'
-import { VersionLabels } from './versionLabels'
-
-const extractSelection = (identifier: string) => {
-  const selection = document.getSelection()
-  return selection && selection?.anchorNode?.parentElement?.closest(`#${identifier}`)
-    ? selection.toString().trim()
-    : undefined
-}
+import VersionCellHeader from './versionCellHeader'
 
 export default function VersionCell<Version extends PromptVersion | ChainVersion>({
   identifier,
@@ -37,18 +24,6 @@ export default function VersionCell<Version extends PromptVersion | ChainVersion
   activeItem: ActivePrompt | ActiveChain
   onSelect: (version: Version) => void
 }) {
-  const [selection, setSelection] = useState<string>()
-  useEffect(() => {
-    const selectionChangeHandler = () => setSelection(extractSelection(identifier))
-    document.addEventListener('selectionchange', selectionChangeHandler)
-    return () => {
-      document.removeEventListener('selectionchange', selectionChangeHandler)
-    }
-  }, [identifier])
-
-  const user = activeItem.users.find(user => user.id === version.userID)
-  const formattedDate = useFormattedDate(version.timestamp, FormatRelativeDate)
-
   return (
     <VerticalBarWrapper
       id={identifier}
@@ -60,35 +35,13 @@ export default function VersionCell<Version extends PromptVersion | ChainVersion
           isActiveVersion ? 'bg-blue-25 border-blue-100' : 'bg-gray-25 border-gray-200 hover:bg-gray-50'
         }`}
         onClick={() => onSelect(version)}>
-        <div className='flex items-center justify-between gap-2 -mb-1'>
-          <div className='flex items-center gap-1 text-xs text-gray-700 whitespace-nowrap'>
-            {user && (
-              <>
-                <UserAvatar user={user} size='sm' />
-                <span className='overflow-hidden font-medium text-ellipsis'>{user.fullName} |</span>
-              </>
-            )}
-            {formattedDate}
-            {version.runs.length > 0 && (
-              <>
-                {' '}
-                • {version.runs.length} {version.runs.length > 1 ? 'responses' : 'response'}
-              </>
-            )}
-          </div>
-          <div className='flex items-center gap-1'>
-            <CommentPopupMenu
-              comments={version.comments.filter(comment => !comment.action)}
-              versionID={version.id}
-              selection={selection}
-              users={activeItem.users}
-              selectedCell={isActiveVersion}
-            />
-            <LabelPopupMenu activeItem={activeItem} item={version} selectedCell={isActiveVersion} />
-            <VersionPopupMenu version={version} selectedCell={isActiveVersion} />
-          </div>
-        </div>
-        <VersionLabels version={version} colors={labelColors} />
+        <VersionCellHeader
+          identifier={identifier}
+          labelColors={labelColors}
+          version={version}
+          isActiveVersion={isActiveVersion}
+          activeItem={activeItem}
+        />
         {IsPromptVersion(version) && (
           <div className={isActiveVersion ? '' : 'line-clamp-2'}>
             <VersionComparison version={version} compareVersion={compareVersion} />
