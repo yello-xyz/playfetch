d9869b27fe89e0e98a3c9c2c90c1d8f29b378e82
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-11-21 17:29:54 +0000

    Add client side memory cache with eviction

---
+++ src/client/cache.ts
@@ -0,0 +1,34 @@
+export default class MemoryCache {
+  cache: Map<string | number, any>
+  capacity: number
+
+  constructor(capacity: number) {
+    this.capacity = capacity
+    this.cache = new Map()
+  }
+
+  get(key: string | number) {
+    if (!this.cache.has(key)) {
+      return undefined
+    }
+  
+    const value = this.cache.get(key)
+    this.cache.delete(key)
+    this.cache.set(key, value)
+  
+    return value
+  }
+
+  set(key: string | number, value: any) {
+    if (this.cache.has(key)) {
+      this.cache.delete(key)
+    }
+  
+    this.cache.set(key, value)
+  
+    if (this.cache.size > this.capacity) {
+      const firstKey = this.cache.keys().next().value  
+      this.cache.delete(firstKey)
+    }
+  }
+}
