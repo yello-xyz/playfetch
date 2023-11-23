6b3a799314850422fc7b474c287f0c5b379eb01c
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-11-23 10:56:52 +0000

    Add simple dialog prompt before revoking access

--- components/settings/teamSettings.tsx
+++ components/settings/teamSettings.tsx
@@ -2,12 +2,22 @@ import { ActiveProject } from '@/types'
 import MembersPane from '../users/membersPane'
 import { useRefreshProject } from '@/src/client/context/refreshContext'
 import api from '@/src/client/api'
+import { useState } from 'react'
+import ModalDialog, { DialogPrompt } from '../modalDialog'
 
 export default function TeamSettings({ activeProject }: { activeProject: ActiveProject }) {
   const refreshProject = useRefreshProject()
+  const [dialogPrompt, setDialogPrompt] = useState<DialogPrompt>()
 
   const inviteMembers = (emails: string[]) => api.inviteToProject(activeProject.id, emails).then(refreshProject)
-  const revokeMemberAccess = (userID: number) => api.revokeProjectAccess(activeProject.id, userID).then(refreshProject)
+
+  const revokeMemberAccess = (userID: number) =>
+    setDialogPrompt({
+      title: 'Are you sure you want to revoke project access for this member?',
+      confirmTitle: 'Proceed',
+      destructive: true,
+      callback: () => api.revokeProjectAccess(activeProject.id, userID).then(refreshProject),
+    })
 
   return (
     <div className='flex flex-col gap-3 p-3 bg-white border border-gray-200 rounded-lg'>
@@ -18,6 +28,7 @@ export default function TeamSettings({ activeProject }: { activeProject: ActiveP
         onInvite={inviteMembers}
         onRevoke={revokeMemberAccess}
       />
+      <ModalDialog prompt={dialogPrompt} onDismiss={() => setDialogPrompt(undefined)} />
     </div>
   )
 }
