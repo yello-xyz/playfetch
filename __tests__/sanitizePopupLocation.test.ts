868f160123e410d0eeb91a60051d020f7050cb9c
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-11-24 17:32:32 +0000

    Add unit tests for global popup location

--- __tests__/sanitizePopupLocation.test.ts
+++ __tests__/sanitizePopupLocation.test.ts
@@ -1,6 +1,9 @@
 import { GlobalPopupLocation } from '@/src/client/context/globalPopupContext'
 import { SanitizePopupLocation } from '@/components/globalPopup'
 
+test(`Test unmounted`, () =>
+  expect(SanitizePopupLocation({ left: 100, right: 100 })[0]).toStrictEqual({ left: 100, right: undefined }))
+
 const parentRect = { width: 800, height: 600 }
 const childRect = { width: 400, height: 300 }
 
@@ -12,3 +15,11 @@ testPopupLocation('Unspecified', {}, { left: 200, top: 150 })
 testPopupLocation('Fully specified', { left: 100, top: 100, right: 100, bottom: 100 })
 testPopupLocation('Top left', { left: 100, top: 100 }, { left: 100, top: 100 })
 testPopupLocation('Bottom right', { right: 100, bottom: 100 }, { right: 700, bottom: 500 })
+testPopupLocation('Adjusts down', { left: 600, top: 400 }, { left: 400, top: 300 })
+testPopupLocation('Adjusts up', { right: 600, bottom: 400 }, { right: 400, bottom: 300 })
+testPopupLocation('Adjusts negative up', { left: -200, top: -200 }, { left: 0, top: 0 })
+testPopupLocation('Adjusts negative down', { right: 1000, bottom: 800 }, { right: 0, bottom: 0 })
+testPopupLocation('Fixed width retains left', { left: 200, right: 600 }, { left: 200, right: 400 })
+testPopupLocation('Fixed width adjusts left', { left: 600, right: 1000 }, { left: 400, right: 0 })
+testPopupLocation('Fixed height retains top', { top: 200, bottom: 500 }, { top: 200, bottom: 300 })
+testPopupLocation('Fixed width adjusts left', { top: 500, bottom: 800 }, { top: 300, bottom: 0 })
