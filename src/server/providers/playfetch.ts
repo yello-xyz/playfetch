a94246476d841fe3c2080ee8d0ae7467417d58ac
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-12-09 14:46:28 +0000

    Implement rate prediction through endpoint

--- src/server/providers/playfetch.ts
+++ src/server/providers/playfetch.ts
@@ -1,35 +1,12 @@
-import { PromptInputs, Prompts, RawPromptVersion } from '@/types'
+import { PromptInputs, RawPromptVersion } from '@/types'
 import { getRecentRatingsForParent } from '../datastore/ratings'
-import getPredictorForDefaultProviderModel from './vertexai'
 import { savePredictedRunRating } from '../datastore/runs'
 import { ensurePromptAccess } from '../datastore/prompts'
 import { getTrustedVersion, savePromptVersionForUser } from '../datastore/versions'
 
-type Rating = Awaited<ReturnType<typeof getRecentRatingsForParent>>[0]
-
-const buildRatingPredictionPrompts = (recentRatings: Rating[], inputs: PromptInputs, output: string) => ({
-  system: 'You are a critical QA tester who is concise and predictable',
-  main: `Given the following ratings for outputs based on inputs, predict the rating for the next output based on its input. Limit yourself to the same ratings and reasons as the ones provided in the examples:
-
-${recentRatings
-  .map(
-    rating =>
-      `Input: ${rating.inputs}
-Output: ${rating.output}
-Rating: ${rating.rating}
-Reason: ${rating.reason}
-
-`
-  )
-  .join('')})}
-
-Inputs: ${inputs}
-Output: ${output}
-Rating: `,
-})
-
 enum Endpoint {
   Respond = '/respond',
+  Rate = '/rate',
   Suggest = '/suggest',
 }
 
@@ -52,44 +29,20 @@ const postRequest = async (endpoint: Endpoint, inputs: PromptInputs, continuatio
   return [output, continuationID] as const
 }
 
-const runPrompt = (prompts: Prompts, temperature = 0) =>
-  getPredictorForDefaultProviderModel('chat-bison')(
-    prompts,
-    temperature,
-    500,
-    {},
-    false,
-    undefined,
-    undefined,
-    undefined,
-    {}
-  ).then(response => ('output' in response && !!response.output ? response.output : null))
-
 export const generateAutoResponse = (system: string, message: string, continuationID?: number) =>
   postRequest(Endpoint.Respond, { system, message }, continuationID)
 
 export async function predictRatingForRun(runID: number, parentID: number, inputs: PromptInputs, output: string) {
   const recentRatings = await getRecentRatingsForParent(parentID)
 
-  if (recentRatings.length > 0) {
-    const response = await runPrompt(buildRatingPredictionPrompts(recentRatings, inputs, output))
-    if (response) {
-      const lines = response
-        .split('\n')
-        .map(line =>
-          line
-            .replace(/Rating:/gi, '')
-            .replace(/Reason:/gi, '')
-            .trim()
-        )
-        .filter(line => line.length > 0)
-      if (lines.length === 2) {
-        const [rating, reason] = lines
-        if (rating === 'positive' || rating === 'negative') {
-          await savePredictedRunRating(runID, rating, reason)
-        }
-      }
-    }
+  const [response] = await postRequest(Endpoint.Rate, {
+    recentRatings: JSON.stringify(recentRatings),
+    inputs: JSON.stringify(inputs),
+    output,
+  })
+
+  if (response?.rating && response?.reason) {
+    await savePredictedRunRating(runID, response.rating, response.reason)
   }
 }
 
@@ -103,7 +56,7 @@ export async function suggestImprovementForPrompt(userID: number, promptID: numb
     prompt: promptVersion.prompts.main,
   })
 
-  if (response) {
+  if (response && typeof response === 'string') {
     await savePromptVersionForUser(
       userID,
       promptID,
