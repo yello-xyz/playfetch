202e654a9545e369ba29ddb1973d80185716b365
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-12-13 15:40:50 +0000

    Only use jsdom test environment when needed
    Otherwise this directive causes issues with other modules like yaml.

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ const createJestConfig = nextJest({ dir: './' })
 
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom'
 }
 
 module.exports = createJestConfig(customJestConfig)
