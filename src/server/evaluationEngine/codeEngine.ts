3163c569dfa16cd60fa542a269f9ae02084b3e26
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-12-13 10:28:34 +0000

    Allow manual message interrupt from code block
    If you don't specify arguments, we will treat the manual interrupt as a message interrupt rather than a function interrupt (i.e. similar to the interrupts from simple chat prompts).

--- src/server/evaluationEngine/codeEngine.ts
+++ src/server/evaluationEngine/codeEngine.ts
@@ -1,5 +1,11 @@
 import { PromptInputs } from '@/types'
-import { CodeModuleName, InterruptOnceFunctionName, ExtractVariables, ToCamelCase } from '@/src/common/formatting'
+import {
+  CodeModuleName,
+  InterruptOnceFunctionName,
+  ExtractVariables,
+  ToCamelCase,
+  DefaultChatContinuationInputKey,
+} from '@/src/common/formatting'
 import Isolated from 'isolated-vm'
 import { EmptyRunResponse, ErrorRunResponse, RunResponse } from './runResponse'
 
@@ -16,24 +22,35 @@ const codeToCamelCase = (code: string) =>
 const stringify = (result: any) => (typeof result === 'object' ? JSON.stringify(result, null, 2) : String(result))
 
 const createCodeContextWithInputs = (inputs: PromptInputs) => {
+  const interruptFlag = '__playfetch_interrupted__'
   const isolated = new Isolated.Isolate({ memoryLimit: 8 })
   const context = isolated.createContextSync()
+  context.global.setSync(interruptFlag, false, { copy: true })
   context.eval(`globalThis.${CodeModuleName} = { 
-    ${InterruptOnceFunctionName}: (name, args) => this[name] ?? { function: { name, arguments: args } } 
+    ${InterruptOnceFunctionName}: (name, args) => {
+      let inputValue = this[args ? name : '${DefaultChatContinuationInputKey}']
+      ${interruptFlag} = !inputValue
+      return inputValue ?? (args ? { function: { name, arguments: args } } : name)
+    }
   }`)
   Object.entries(inputs).forEach(([variable, value]) =>
     context.global.setSync(ToCamelCase(variable), value, { copy: true })
   )
-  return context
+  return [context, () => context.global.getSync(interruptFlag) as boolean] as const
 }
 
 export const runCodeWithInputs = async (code: string, inputs: PromptInputs): Promise<RunResponse> => {
   try {
-    const context = createCodeContextWithInputs(inputs)
+    const [context, interrupted] = createCodeContextWithInputs(inputs)
     const functionCode = `(() => { ${codeToCamelCase(code)} })()`
     const result = await context.eval(functionCode, { timeout: 1000, copy: true })
     const output = stringify(result)
-    return { ...EmptyRunResponse(), result, output, functionCall: result?.function?.name ?? null }
+    return {
+      ...EmptyRunResponse(),
+      result,
+      output,
+      functionCall: interrupted() ? result?.function?.name ?? DefaultChatContinuationInputKey : null,
+    }
   } catch (error: any) {
     return ErrorRunResponse(error.message)
   }
