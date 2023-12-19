6c2aaeb0511a578c9b7be9938756ed8c897a8301
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-12-19 16:09:15 +0000

    Delete recent ratings when deleting prompt/chain

--- src/server/datastore/cleanup.ts
+++ src/server/datastore/cleanup.ts
@@ -1,5 +1,5 @@
 import { Key } from '@google-cloud/datastore'
-import { Entity, buildKey, getDatastore, getEntityKeys, getID } from './datastore'
+import { Entity, buildKey, getDatastore, getEntityKey, getEntityKeys, getID, getKeyedEntity } from './datastore'
 
 export async function migrateCleanup(postMerge: boolean) {
   if (postMerge) {
@@ -36,11 +36,12 @@ export default async function cleanUpEntities() {
     const entityID = getID(cleanupData)
     switch (cleanupData.entity) {
       case Entity.ENDPOINT:
-        await datastore.delete(buildKey(Entity.USAGE, entityID))
+        await deleteSingleEntity(Entity.USAGE, entityID)
         await deleteBatchedEntities(Entity.LOG, 'endpointID', entityID)
         break
       case Entity.PROMPT:
       case Entity.CHAIN:
+        await deleteSingleEntity(Entity.RATING, entityID)
         await deleteBatchedEntities(Entity.CACHE, 'parentID', entityID)
         await deleteBatchedEntities(Entity.INPUT, 'parentID', entityID)
         await deleteBatchedEntities(Entity.COMMENT, 'parentID', entityID)
@@ -53,10 +54,19 @@ export default async function cleanUpEntities() {
   await datastore.delete(allCleanupData.map(cleanupData => cleanupData[datastore.KEY]))
 }
 
+const deleteSingleEntity = async (type: Entity, entityID: number) => {
+  const entity = await getKeyedEntity(type, entityID)
+  if (entity) {
+    console.log(`Cleaning up ${type} entity with ID ${entityID}…`)
+    await getDatastore().delete(buildKey(type, entityID))
+  }
+}
+
 const deleteBatchedEntities = async (type: Entity, parentKey: string, parentID: number) => {
   while (true) {
     const entityKeys = await getEntityKeys(type, parentKey, parentID)
     if (entityKeys.length > 0) {
+      console.log(`Cleaning up ${entityKeys.length} ${type} entities for ${parentKey} ${parentID}…`)
       await getDatastore().delete(entityKeys)
     } else {
       return
