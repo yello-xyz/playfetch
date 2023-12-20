28e9d2aad67146dd28bb44d13772aae417c9419f
Hannes Verlinde <hannes.verlinde@gmail.com>
2023-12-20 14:38:46 +0000

    Update TODO
    Originally (day after keynote) the OpenAI documentation stated that gpt-3.5-turbo-16k would start pointing to the new gpt-3.5-turbo-1106 on December 11th but it is still pointing to gpt-3.5-turbo-16k-0613 instead and the reference to December 11th was removed from their documentation page.

--- src/server/providers/openai.ts
+++ src/server/providers/openai.ts
@@ -91,7 +91,7 @@ async function tryCompleteChat(
     const response = await api.chat.completions.create(
       {
         model: model
-          // TODO remove this after Dec 11th (when the former points to the latter)
+          // TODO remove this when the former points to the latter (was initially going to be Dec 11th but is not)
           .replaceAll('gpt-3.5-turbo-16k', 'gpt-3.5-turbo-1106')
           // TODO remove this once the model is generally available (also update model description)
           .replaceAll('gpt-4-turbo', 'gpt-4-1106-preview'),
