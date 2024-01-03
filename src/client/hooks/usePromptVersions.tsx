a197d73de0f353bed30e43cd36b4fc7a4b57dd5a
Hannes Verlinde <hannes.verlinde@gmail.com>
2024-01-03 16:46:16 +0100

    Mark modified version as not run
    This is cleaner than only having the correct value for the didRun flag in the versions array and a different (wrong) one in the exposed currentVersion property even if the latter isn't really used in practice.

--- src/client/hooks/usePromptVersions.tsx
+++ src/client/hooks/usePromptVersions.tsx
@@ -9,14 +9,13 @@ export default function usePromptVersions(
 ) {
   const [currentVersion, setCurrentVersion] = useInitialState(activeVersion, (a, b) => a.id === b.id)
   const updateVersion = (version: PromptVersion) => {
-    setCurrentVersion(version)
-    setModifiedVersion(version)
+    const modifiedVersion = { ...version, didRun: false }
+    setCurrentVersion(modifiedVersion)
+    setModifiedVersion(modifiedVersion)
   }
 
   const isDirty = !PromptVersionsAreEqual(activeVersion, currentVersion)
-  const versions = isDirty
-    ? [...prompt.versions.filter(version => version.didRun), { ...currentVersion, didRun: false }]
-    : prompt.versions
+  const versions = isDirty ? [...prompt.versions.filter(version => version.didRun), currentVersion] : prompt.versions
 
   return [versions, updateVersion, currentVersion, isDirty] as const
 }
