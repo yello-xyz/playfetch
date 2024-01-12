6001cee2767a75294740853eb5735e4becd9908f
Hannes Verlinde <hannes.verlinde@gmail.com>
2024-01-12 11:20:58 +0000

    Make expanded test data pane resizable
    This means it won't adjust nicely to its content anymore (when it does fit) cause that's the same trade-off we had before with the prompt panel (unless we do some awful hacking).
    
    Close #133

--- components/prompts/promptView.tsx
+++ components/prompts/promptView.tsx
@@ -70,73 +70,80 @@ export default function PromptView({
   )
 
   const minWidth = 400
+  const minTopPaneHeight = 120
+  const minBottomPaneHeight = testDataExpanded ? 195 : 94
+  const maxBottomPaneHeight = testDataExpanded ? Infinity : 94
   return (
-    <div className='flex flex-col h-full'>
-      <Allotment className='flex-1 bg-gray-25'>
-        <Allotment.Pane minSize={minWidth} preferredSize='50%'>
-          <PromptTabs
-            prompt={prompt}
-            activeVersion={activeVersion}
-            setActiveVersion={setActiveVersion}
-            currentVersion={currentVersion}
-            updatePrompt={updatePrompt}
-            updateConfig={updateConfig}
-          />
-        </Allotment.Pane>
-        <Allotment.Pane minSize={minWidth}>
-          <div className='flex flex-col h-full border-l border-gray-200'>
-            <SingleTabHeader label='Responses' />
-            <div className='flex-1 min-h-0 overflow-y-auto'>
-              <RunTimeline
-                runs={[...activeVersion.runs, ...partialRuns]}
-                activeItem={prompt}
-                version={activeVersion}
-                focusRunID={focusRunID}
-                runVersion={runPrompt}
-                selectInputValue={SelectAnyInputValue(inputValues, testConfig)}
-                isRunning={isRunning}
-                skipHeader
-              />
-            </div>
-          </div>
-        </Allotment.Pane>
-      </Allotment>
-      <div className='border-t border-gray-200' />
-      {canShowTestData && (
-        <div className='max-h-[50%] overflow-y-auto'>
-          <Collapsible
-            key={testDataExpanded.toString()}
-            title='Test Data'
-            initiallyExpanded={testDataExpanded}
-            className='mt-1.5 border-t border-gray-200'
-            titleClassName='pt-1.5 pl-0.5'
-            rightHandItems={<IconButton className='mr-3 rounded' icon={expandIcon} onClick={expandTestData} />}>
-            <TestDataPane
-              variables={variables}
-              staticVariables={staticVariables}
-              inputValues={inputValues}
-              setInputValues={setInputValues}
-              persistInputValuesIfNeeded={persistInputValuesIfNeeded}
-              testConfig={testConfig}
-              setTestConfig={setTestConfig}
+    <Allotment vertical>
+      <Allotment.Pane minSize={minTopPaneHeight}>
+        <Allotment className='flex-1 bg-gray-25'>
+          <Allotment.Pane minSize={minWidth} preferredSize='50%'>
+            <PromptTabs
+              prompt={prompt}
+              activeVersion={activeVersion}
+              setActiveVersion={setActiveVersion}
+              currentVersion={currentVersion}
+              updatePrompt={updatePrompt}
+              updateConfig={updateConfig}
             />
-          </Collapsible>
+          </Allotment.Pane>
+          <Allotment.Pane minSize={minWidth}>
+            <div className='flex flex-col h-full border-l border-gray-200'>
+              <SingleTabHeader label='Responses' />
+              <div className='flex-1 min-h-0 overflow-y-auto'>
+                <RunTimeline
+                  runs={[...activeVersion.runs, ...partialRuns]}
+                  activeItem={prompt}
+                  version={activeVersion}
+                  focusRunID={focusRunID}
+                  runVersion={runPrompt}
+                  selectInputValue={SelectAnyInputValue(inputValues, testConfig)}
+                  isRunning={isRunning}
+                  skipHeader
+                />
+              </div>
+            </div>
+          </Allotment.Pane>
+        </Allotment>
+      </Allotment.Pane>
+      <Allotment.Pane minSize={minBottomPaneHeight} maxSize={maxBottomPaneHeight} className='flex flex-col'>
+        {canShowTestData && (
+          <div className='flex-1 min-h-0 overflow-y-auto'>
+            <Collapsible
+              key={testDataExpanded.toString()}
+              title='Test Data'
+              initiallyExpanded={testDataExpanded}
+              className='mt-1.5 border-t border-gray-200'
+              titleClassName='pt-1.5 pl-0.5'
+              rightHandItems={<IconButton className='mr-3 rounded' icon={expandIcon} onClick={expandTestData} />}
+              onSetExpanded={setTestDataExpanded}>
+              <TestDataPane
+                variables={variables}
+                staticVariables={staticVariables}
+                inputValues={inputValues}
+                setInputValues={setInputValues}
+                persistInputValuesIfNeeded={persistInputValuesIfNeeded}
+                testConfig={testConfig}
+                setTestConfig={setTestConfig}
+              />
+            </Collapsible>
+          </div>
+        )}
+        <div className='p-4'>
+          <RunButtons
+            runTitle={activeVersion.runs.length > 0 && !isDirty ? 'Run again' : 'Run'}
+            variables={variables}
+            staticVariables={staticVariables}
+            inputValues={inputValues}
+            testConfig={testConfig}
+            setTestConfig={setTestConfig}
+            onShowTestConfig={() => setTestDataExpanded(true)}
+            disabled={!isModelAvailable || !VersionHasNonEmptyPrompts(currentVersion) || isRunning}
+            callback={saveAndRun}
+            onSave={isDirty ? savePrompt : undefined}
+          />
         </div>
-      )}
-      <div className='p-4'>
-        <RunButtons
-          runTitle={activeVersion.runs.length > 0 && !isDirty ? 'Run again' : 'Run'}
-          variables={variables}
-          staticVariables={staticVariables}
-          inputValues={inputValues}
-          testConfig={testConfig}
-          setTestConfig={setTestConfig}
-          onShowTestConfig={() => setTestDataExpanded(true)}
-          disabled={!isModelAvailable || !VersionHasNonEmptyPrompts(currentVersion) || isRunning}
-          callback={saveAndRun}
-          onSave={isDirty ? savePrompt : undefined}
-        />
-      </div>
-    </div>
+      </Allotment.Pane>
+    </Allotment>
   )
 }
