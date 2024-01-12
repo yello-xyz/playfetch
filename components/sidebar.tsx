9419b88175a820d17343b560d2a6d4be27c083e4
Stef Smet <stef.smet@gmail.com>
2024-01-12 18:55:22 +0000

    adjust padding

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -43,7 +43,7 @@ export function SidebarSection({
   return (
     <div className={`${className} flex flex-col gap-0.5`}>
       {title && (
-        <div className='flex items-center justify-between p-1 pl-4 text-xs font-medium text-gray-400'>
+        <div className='flex items-center justify-between p-1 pl-2.5 pr-0 text-xs font-medium text-gray-400'>
           {title}
           {actionComponent}
         </div>
