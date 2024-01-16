c8a35e1efd2d10a24aed01f307249ff8a903b4b0
Hannes Verlinde <hannes.verlinde@gmail.com>
2024-01-16 15:26:10 +0000

    Add hooks for floating sidebar and prompt tabs

--- src/client/context/userPresetsContext.tsx
+++ src/client/context/userPresetsContext.tsx
@@ -25,3 +25,32 @@ export function useDefaultPromptConfig() {
 
   return [currentUserPresets.defaultPromptConfig, updateDefaultModel, updateDefaultParameters] as const
 }
+
+function useLayoutConfig() {
+  const context = useContext(UserPresetsContext)
+
+  const currentUserPresets = context.currentUserPresets!
+
+  const updateConfig = (config: Partial<UserPresets['layoutConfig']>) =>
+    api
+      .updateLayoutConfig(config)
+      .then(layoutConfig => context.setCurrentUserPresets!({ ...currentUserPresets, layoutConfig }))
+
+  const floatingSidebar = currentUserPresets.layoutConfig.floatingSidebar
+  const setFloatingSidebar = (floatingSidebar: boolean) => updateConfig({ floatingSidebar })
+  
+  const splitPromptTabs = currentUserPresets.layoutConfig.splitPromptTabs
+  const setSplitPromptTabs = (splitPromptTabs: boolean) => updateConfig({ splitPromptTabs })
+
+  return [floatingSidebar, splitPromptTabs, setFloatingSidebar, setSplitPromptTabs] as const
+}
+
+export function useFloatingSidebar() {
+  const [floatingSidebar, _, setFloatingSidebar] = useLayoutConfig()
+  return [floatingSidebar, setFloatingSidebar] as const
+}
+
+export function useSplitPromptTabs() {
+  const [_, splitPromptTabs, __, setSplitPromptTabs] = useLayoutConfig()
+  return [splitPromptTabs, setSplitPromptTabs] as const
+}
