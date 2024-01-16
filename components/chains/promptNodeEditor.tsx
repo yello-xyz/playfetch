6b59158232315feda76539436ac7e7ea0f327f7d
Hannes Verlinde <hannes.verlinde@gmail.com>
2024-01-16 11:14:38 +0000

    Fix build
    PromptTabs component is used here as well so we need to store the stat in the parent (even though we don't need to adapt the preferred size of any allotment here).

--- components/chains/promptNodeEditor.tsx
+++ components/chains/promptNodeEditor.tsx
@@ -2,6 +2,7 @@ import { ActivePrompt, PromptChainItem, PromptVersion } from '@/types'
 import usePromptVersion from '@/src/client/hooks/usePromptVersion'
 import { ChainPromptCache } from '../../src/client/hooks/useChainPromptCache'
 import PromptTabs from '../prompts/promptTabs'
+import { useState } from 'react'
 
 export default function PromptNodeEditor({
   item,
@@ -40,6 +41,7 @@ function PromptEditor({
   selectVersion: (version: PromptVersion) => void
   setModifiedVersion: (version: PromptVersion) => void
 }) {
+  const [areTabsMerged, setTabsMerged] = useState(true)
   const [currentVersion, versions, updatePrompt, updateConfig] = usePromptVersion(
     prompt,
     activeVersion,
@@ -55,6 +57,8 @@ function PromptEditor({
       currentVersion={currentVersion}
       updatePrompt={updatePrompt}
       updateConfig={updateConfig}
+      areTabsMerged={areTabsMerged}
+      setTabsMerged={setTabsMerged}
     />
   )
 }
