2322fbc032c9278cd0a903c7c693e2dc9ce95267
Hannes Verlinde <hannes.verlinde@gmail.com>
2024-01-19 11:07:55 +0000

    Factor out LinkedTableItem component

--- src/client/hooks/useTestDataActionButtons.tsx
+++ src/client/hooks/useTestDataActionButtons.tsx
@@ -1,8 +1,8 @@
-import { Dispatch, SetStateAction, useState } from 'react'
+import { Dispatch, MouseEvent, SetStateAction, useState } from 'react'
 import useGlobalPopup, { WithDismiss } from '@/src/client/context/globalPopupContext'
 import PopupMenu, { PopupContent, PopupMenuItem } from '../../../components/popupMenu'
 import TestDataPane from '@/components/testData/testDataPane'
-import { Chain, InputValues, ProjectItemIsChain, Prompt, TestConfig } from '@/types'
+import { Chain, InputValues, ProjectItemIsChain, Prompt, Table, TestConfig } from '@/types'
 import Label from '@/components/label'
 import IconButton from '@/components/iconButton'
 import closeIcon from '@/public/close.svg'
@@ -47,7 +47,6 @@ export default function useTestDataActionButtons(
   }
 
   const tables = activeProject.tables
-  const table = tables.find(table => table.id === parentItem.tableID)
   const isDataEmpty = Object.values(inputValues).every(value => value.length === 0 || value[0] === '')
   const canReplaceData = tables.length > 0 && (!parentItem.tableID || tables.length > 1)
   const onReplaceData = canReplaceData ? () => {} : undefined
@@ -55,19 +54,7 @@ export default function useTestDataActionButtons(
   const actionButtons = (className = '') => (
     <div className={`${className} relative grow flex items-center gap-1`}>
       <div className='flex flex-1'>
-        {table && (
-          <div
-            className={`flex items-center gap-1 pl-1 pr-2 bg-gray-100 rounded ${
-              onReplaceData ? 'cursor-pointer hover:bg-gray-200' : ''
-            }`}
-            onClick={event => {
-              event.stopPropagation()
-              onReplaceData
-            }}>
-            <Icon icon={tableIcon} />
-            {table.name}
-          </div>
-        )}
+        <LinkedTableItem table={tables.find(table => table.id === parentItem.tableID)} onReplaceData={onReplaceData} />
       </div>
       <IconButton icon={expandIcon} onClick={expandTestData} />
       <IconButton icon={dotsIcon} className='rotate-90' onClick={() => setMenuExpanded(!isMenuExpanded)} />
@@ -80,6 +67,21 @@ export default function useTestDataActionButtons(
   )
 
   return actionButtons
+}
+
+function LinkedTableItem({ table, onReplaceData }: { table?: Table; onReplaceData?: () => void }) {
+  const cursorClass = onReplaceData ? 'cursor-pointer hover:bg-gray-200' : ''
+  const onClick = (event: MouseEvent) => {
+    event.stopPropagation()
+    onReplaceData
+  }
+
+  return table ? (
+    <div className={`flex items-center gap-1 pl-1 pr-2 bg-gray-100 rounded ${cursorClass}`} onClick={onClick}>
+      <Icon icon={tableIcon} />
+      {table.name}
+    </div>
+  ) : null
 }
 
 type TestDataPopupProps = {
