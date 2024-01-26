1615e0f55cd9892cd08480dcad64cce07f172827
Hannes Verlinde <hannes.verlinde@gmail.com>
2024-01-26 11:39:26 +0000

    Factor out LabelConfigurationPanes
    We also want to show these when Linear is already configured at project level but without edit buttons.

--- components/settings/linearSettings.tsx
+++ components/settings/linearSettings.tsx
@@ -11,6 +11,8 @@ import LabelPopupMenu, { AvailableLabelColorsForItem } from '../labelPopupMenu'
 import IconButton from '../iconButton'
 import cancelIcon from '@/public/cancel.svg'
 
+type Trigger = [string[], string[]]
+
 export default function LinearSettings({
   activeProject,
   scopeID,
@@ -25,19 +27,13 @@ export default function LinearSettings({
   const router = useRouter()
   const availableProvider = useIssueTrackerProvider()
 
-  const defaultLabels: [string[], string[]] = [[NeedsUpdatesLabel], [NeedsUpdatesLabel]]
-  const availableLabels = activeProject ? activeProject.availableLabels : []
-  const labelColors = activeProject ? AvailableLabelColorsForItem(activeProject) : {}
+  const defaultLabels: Trigger = [[NeedsUpdatesLabel], [NeedsUpdatesLabel]]
 
   const scopedProvider = provider as AvailableIssueTrackerProvider | undefined
   const previousEnvironment = scopedProvider?.environment
-  const scopedLabels: (typeof defaultLabels)[] = previousEnvironment ? JSON.parse(previousEnvironment) : [defaultLabels]
+  const scopedLabels: Trigger[] = previousEnvironment ? JSON.parse(previousEnvironment) : [defaultLabels]
 
   const [labels, setLabels] = useState(scopedLabels)
-  const toggle = (labels: string[], label: string) =>
-    labels.includes(label) ? labels.filter(l => l !== label) : [...labels, label]
-
-  const gridConfig = 'w-full grid grid-cols-[180px_minmax(120px,1fr)_24px] items-start gap-1'
 
   return (
     <AppSettings
@@ -55,63 +51,23 @@ export default function LinearSettings({
       )}
       projectConfiguration={(isConfigured, isUpdating, isProcessing, confirmButton) => (
         <>
-          {isConfigured &&
-            labels.map(([triggers, toggles], index) => (
-              <div className='flex flex-col gap-1' key={index}>
-                <span>Create task on adding label: {triggers.join(', ')}</span>
-                <span>Toggle labels on closing task: {toggles.join(', ')}</span>
-              </div>
-            ))}
-          {isUpdating ? (
+          {isConfigured || isUpdating ? (
             <div className='flex flex-col w-full gap-2.5'>
-              {labels.map(([triggers, toggles], index) => (
-                <div className='flex items-center gap-2'>
-                  <IconButton icon={cancelIcon} onClick={() => setLabels(labels.filter((_, i) => i !== index))} />
-                  <div className={`px-4 py-2 bg-gray-50 border border-gray-200 rounded-lg ${gridConfig}`} key={index}>
-                    <GridCell className='font-medium'>Create task on adding label:</GridCell>
-                    <GridCell>
-                      <ItemLabels labels={triggers} colors={labelColors} />
-                    </GridCell>
-                    <GridCell>
-                      {!isProcessing && (
-                        <LabelPopupMenu
-                          activeLabels={triggers}
-                          availableLabels={availableLabels}
-                          labelColors={labelColors}
-                          toggleLabel={l =>
-                            setLabels(labels.map(([t, _], i) => (i === index ? [toggle(t, l), toggles] : [t, toggles])))
-                          }
-                        />
-                      )}
-                    </GridCell>
-                    <GridCell className='font-medium'>Toggle labels on closing task:</GridCell>
-                    <GridCell>
-                      <ItemLabels labels={toggles} colors={labelColors} />
-                    </GridCell>
-                    <GridCell>
-                      {!isProcessing && (
-                        <LabelPopupMenu
-                          activeLabels={toggles}
-                          availableLabels={availableLabels}
-                          labelColors={labelColors}
-                          toggleLabel={l =>
-                            setLabels(
-                              labels.map(([_, t], i) => (i === index ? [triggers, toggle(t, l)] : [triggers, t]))
-                            )
-                          }
-                        />
-                      )}
-                    </GridCell>
-                  </div>
-                </div>
-              ))}
+              <LabelConfigurationPanes {...{ labels, setLabels, activeProject, isUpdating, isProcessing }} />
               <div className='flex gap-2.5 justify-end'>
-                <Button type='secondary' disabled={isProcessing} onClick={() => setLabels([...labels, defaultLabels])}>
-                  Add Trigger
-                </Button>
-                <Button type='destructive' disabled={isProcessing} onClick={() => setLabels(scopedLabels)}>
-                  Revert
-                </Button>
+                {isUpdating && (
+                  <>
+                    <Button
+                      type='secondary'
+                      disabled={isProcessing}
+                      onClick={() => setLabels([...labels, defaultLabels])}>
+                      Add Trigger
+                    </Button>
+                    <Button type='destructive' disabled={isProcessing} onClick={() => setLabels(scopedLabels)}>
+                      Revert
+                    </Button>
+                  </>
+                )}
                 {confirmButton()}
               </div>
             </div>
@@ -121,6 +77,74 @@ export default function LinearSettings({
         </>
       )}
     />
+  )
+}
+
+const LabelConfigurationPanes = ({
+  labels,
+  setLabels,
+  activeProject,
+  isUpdating,
+  isProcessing,
+}: {
+  labels: Trigger[]
+  setLabels: (labels: Trigger[]) => void
+  activeProject?: ActiveProject
+  isUpdating: boolean
+  isProcessing: boolean
+}) => {
+  const availableLabels = activeProject ? activeProject.availableLabels : []
+  const labelColors = activeProject ? AvailableLabelColorsForItem(activeProject) : {}
+
+  const toggle = (labels: string[], label: string) =>
+    labels.includes(label) ? labels.filter(l => l !== label) : [...labels, label]
+
+  const gridConfig = 'w-full grid grid-cols-[180px_minmax(120px,1fr)_24px] items-start gap-1'
+
+  return (
+    <>
+      {labels.map(([triggers, toggles], index) => (
+        <div className='flex items-center gap-2'>
+          {isUpdating && (
+            <IconButton icon={cancelIcon} onClick={() => setLabels(labels.filter((_, i) => i !== index))} />
+          )}
+          <div className={`px-4 py-2 bg-gray-50 border border-gray-200 rounded-lg ${gridConfig}`} key={index}>
+            <GridCell className='font-medium'>Create task on adding label:</GridCell>
+            <GridCell>
+              <ItemLabels labels={triggers} colors={labelColors} />
+            </GridCell>
+            <GridCell>
+              {isUpdating && !isProcessing && (
+                <LabelPopupMenu
+                  activeLabels={triggers}
+                  availableLabels={availableLabels}
+                  labelColors={labelColors}
+                  toggleLabel={l =>
+                    setLabels(labels.map(([t, _], i) => (i === index ? [toggle(t, l), toggles] : [t, toggles])))
+                  }
+                />
+              )}
+            </GridCell>
+            <GridCell className='font-medium'>Toggle labels on closing task:</GridCell>
+            <GridCell>
+              <ItemLabels labels={toggles} colors={labelColors} />
+            </GridCell>
+            <GridCell>
+              {isUpdating && !isProcessing && (
+                <LabelPopupMenu
+                  activeLabels={toggles}
+                  availableLabels={availableLabels}
+                  labelColors={labelColors}
+                  toggleLabel={l =>
+                    setLabels(labels.map(([_, t], i) => (i === index ? [triggers, toggle(t, l)] : [triggers, t])))
+                  }
+                />
+              )}
+            </GridCell>
+          </div>
+        </div>
+      ))}
+    </>
   )
 }
 
