bbc417b62b5d728da8286a0dc2b3915890ec8dde
Hannes Verlinde <hannes.verlinde@gmail.com>
2024-01-26 11:19:25 +0000

    Allow to delete trigger in Linear settings

--- components/settings/linearSettings.tsx
+++ components/settings/linearSettings.tsx
@@ -8,6 +8,8 @@ import { ReactNode, useState } from 'react'
 import { NeedsUpdatesLabel } from '@/src/common/defaults'
 import { ItemLabels } from '../versions/versionLabels'
 import LabelPopupMenu, { AvailableLabelColorsForItem } from '../labelPopupMenu'
+import IconButton from '../iconButton'
+import cancelIcon from '@/public/cancel.svg'
 
 export default function LinearSettings({
   activeProject,
@@ -61,41 +63,46 @@ export default function LinearSettings({
               </div>
             ))}
           {isUpdating ? (
-            <div className='flex flex-col w-full gap-2'>
+            <div className='flex flex-col w-full gap-2.5'>
               {labels.map(([triggers, toggles], index) => (
-                <div className={`px-4 py-2 bg-gray-50 border border-gray-200 rounded-lg ${gridConfig}`} key={index}>
-                  <GridCell className='font-medium'>Create task on adding label:</GridCell>
-                  <GridCell>
-                    <ItemLabels labels={triggers} colors={labelColors} />
-                  </GridCell>
-                  <GridCell>
-                    {!isProcessing && (
-                      <LabelPopupMenu
-                        activeLabels={triggers}
-                        availableLabels={availableLabels}
-                        labelColors={labelColors}
-                        toggleLabel={l =>
-                          setLabels(labels.map(([t, _], i) => (i === index ? [toggle(t, l), toggles] : [t, toggles])))
-                        }
-                      />
-                    )}
-                  </GridCell>
-                  <GridCell className='font-medium'>Toggle labels on closing task:</GridCell>
-                  <GridCell>
-                    <ItemLabels labels={toggles} colors={labelColors} />
-                  </GridCell>
-                  <GridCell>
-                    {!isProcessing && (
-                      <LabelPopupMenu
-                        activeLabels={toggles}
-                        availableLabels={availableLabels}
-                        labelColors={labelColors}
-                        toggleLabel={l =>
-                          setLabels(labels.map(([_, t], i) => (i === index ? [triggers, toggle(t, l)] : [triggers, t])))
-                        }
-                      />
-                    )}
-                  </GridCell>
+                <div className='flex items-center gap-2'>
+                  <IconButton icon={cancelIcon} onClick={() => setLabels(labels.filter((_, i) => i !== index))} />
+                  <div className={`px-4 py-2 bg-gray-50 border border-gray-200 rounded-lg ${gridConfig}`} key={index}>
+                    <GridCell className='font-medium'>Create task on adding label:</GridCell>
+                    <GridCell>
+                      <ItemLabels labels={triggers} colors={labelColors} />
+                    </GridCell>
+                    <GridCell>
+                      {!isProcessing && (
+                        <LabelPopupMenu
+                          activeLabels={triggers}
+                          availableLabels={availableLabels}
+                          labelColors={labelColors}
+                          toggleLabel={l =>
+                            setLabels(labels.map(([t, _], i) => (i === index ? [toggle(t, l), toggles] : [t, toggles])))
+                          }
+                        />
+                      )}
+                    </GridCell>
+                    <GridCell className='font-medium'>Toggle labels on closing task:</GridCell>
+                    <GridCell>
+                      <ItemLabels labels={toggles} colors={labelColors} />
+                    </GridCell>
+                    <GridCell>
+                      {!isProcessing && (
+                        <LabelPopupMenu
+                          activeLabels={toggles}
+                          availableLabels={availableLabels}
+                          labelColors={labelColors}
+                          toggleLabel={l =>
+                            setLabels(
+                              labels.map(([_, t], i) => (i === index ? [triggers, toggle(t, l)] : [triggers, t]))
+                            )
+                          }
+                        />
+                      )}
+                    </GridCell>
+                  </div>
                 </div>
               ))}
               <div className='flex gap-2.5 justify-end'>
