dbb2357f9ff6f382e8551af05c9a5b3ae4011d94
Hannes Verlinde <hannes.verlinde@gmail.com>
2024-04-23 09:28:40 +0100

    Factor out getAPIBaseURL helper

--- src/server/routing.ts
+++ src/server/routing.ts
@@ -1,3 +1,5 @@
+export const getAPIBaseURL = () => process.env.API_URL || process.env.NEXTAUTH_URL
+
 const buildURLForRoute = (route: string) => `${process.env.NEXTAUTH_URL}${route}`
 
 export default buildURLForRoute
